88f0e9f165ab89f09504b0fbb49fbc96381a6890
Alex Li <Alextrovert@gmail.com>
2015-05-27 19:26:57 -0400

    Update 1.4 Topological Sorting.cpp

--- Section-1-Graph-Theory/1.4 Topological Sorting.cpp
+++ Section-1-Graph-Theory/1.4 Topological Sorting.cpp
@@ -35,10 +35,10 @@ The topological order:
 #include <vector>
 using namespace std;
 
-const int MAX_N = 100;
+const int MAXN = 100;
 int nodes, edges, a, b;
-bool done[MAX_N+1] = {0}, visit[MAX_N+1] = {0}; 
-vector<int> adj[MAX_N+1], sorted;
+bool done[MAXN+1] = {0}, visit[MAXN+1] = {0}; 
+vector<int> adj[MAXN+1], sorted;
 
 void DFS(int node) {
   if (visit[node]) {
