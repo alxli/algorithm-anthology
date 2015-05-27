e21b391b8d39efec8216e6e91933c8a65f223390
Alex Li <Alextrovert@gmail.com>
2015-05-27 19:23:14 -0400

    Update 1.5.2 Bellman-Ford Algorithm.cpp

--- Section-1-Graph-Theory/1.5.2 Bellman-Ford Algorithm.cpp
+++ Section-1-Graph-Theory/1.5.2 Bellman-Ford Algorithm.cpp
@@ -8,52 +8,6 @@ a single starting node. The input graph is stored using an edge list.
 
 Complexity: O(V*E) on the number of vertices and edges, respectively.
 
-*/
-
-#include <iostream>
-using namespace std;
-
-const int MAX_N=101, MAX_E = MAX_N*MAX_N, INF=1<<28;
-int nodes, edges, a, b, weight, start, dest;
-int E[MAX_E][3], dist[MAX_N], pred[MAX_N];
-
-int main() {
-    cin >> nodes >> edges;
-    for (int i = 0; i < edges; i++)
-        cin >> E[i][0] >> E[i][1] >> E[i][2];
-    cin >> start >> dest;
-
-    for (int i = 1; i <= nodes; i++) {
-        dist[i] = INF;
-        pred[i] = -1;
-    }
-    dist[start] = 0;
-    for (int i = 1; i <= nodes; i++)
-      for (int j = 0; j < edges; j++)
-        if (dist[E[j][1]] > dist[E[j][0]] + E[j][2]) {
-            dist[E[j][1]] = dist[E[j][0]] + E[j][2];
-            pred[E[j][1]] = E[j][0];
-        }
-    cout << "The shortest path from " << start;
-    cout << " to " << dest << " is ";
-    cout << dist[dest] << "." << endl;
-
-    /* Optional: Report negative-weight cycles */
-    for (int i = 0; i < edges; i++) 
-      if (dist[E[i][0]] + E[i][2] < dist[E[i][1]])
-        cout << "Negative-weight cycle detected!\n";
-
-    /* Use pred[] to backtrack and print the path */
-    int i = 0, j = dest, path[MAX_N + 1];
-    while (pred[j] != -1) j = path[++i] = pred[j];
-    cout << "Take the path: ";
-    while (i > 0) cout << path[i--] << "->";
-    cout << dest << ".\n";
-    return 0;
-}
-
-/*
-
 =~=~=~=~= Sample Input =~=~=~=~=
 3 3
 1 2 1
@@ -66,3 +20,44 @@ The shortest path from 1 to 3 is 3.
 Take the path 1->2->3. 
 
 */
+
+#include <iostream>
+using namespace std;
+
+const int MAX_N=100, MAX_E = MAX_N*MAX_N, INF=1<<28;
+int nodes, edges, a, b, weight, start, dest;
+int E[MAX_E][3], dist[MAX_N+1], pred[MAX_N+1];
+
+int main() {
+  cin >> nodes >> edges;
+  for (int i = 0; i < edges; i++)
+    cin >> E[i][0] >> E[i][1] >> E[i][2];
+  cin >> start >> dest;
+  for (int i = 1; i <= nodes; i++) {
+    dist[i] = INF;
+    pred[i] = -1;
+  }
+  dist[start] = 0;
+  for (int i = 1; i <= nodes; i++)
+    for (int j = 0; j < edges; j++)
+      if (dist[E[j][1]] > dist[E[j][0]] + E[j][2]) {
+          dist[E[j][1]] = dist[E[j][0]] + E[j][2];
+          pred[E[j][1]] = E[j][0];
+      }
+  cout << "The shortest path from " << start;
+  cout << " to " << dest << " is ";
+  cout << dist[dest] << "." << endl;
+
+  /* Optional: Report negative-weight cycles */
+  for (int i = 0; i < edges; i++) 
+    if (dist[E[i][0]] + E[i][2] < dist[E[i][1]])
+      cout << "Negative-weight cycle detected!\n";
+
+  /* Use pred[] to backtrack and print the path */
+  int i = 0, j = dest, path[MAX_N + 1];
+  while (pred[j] != -1) j = path[++i] = pred[j];
+  cout << "Take the path: ";
+  while (i > 0) cout << path[i--] << "->";
+  cout << dest << ".\n";
+  return 0;
+}
