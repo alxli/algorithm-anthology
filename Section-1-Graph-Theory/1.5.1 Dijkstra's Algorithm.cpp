23e11e87c3819972df7a33df8212aa54e7d8d1c1
Alex Li <Alextrovert@gmail.com>
2015-05-27 19:19:18 -0400

    Update 1.5.1 Dijkstra's Algorithm.cpp

--- Section-1-Graph-Theory/1.5.1 Dijkstra's Algorithm.cpp
+++ Section-1-Graph-Theory/1.5.1 Dijkstra's Algorithm.cpp
@@ -7,63 +7,6 @@ of vertices and E is the number of edges. Using an adjacency list and
 priority queue (internally a binary heap), the implementation here is
 Θ((E+V) log(V)), dominated by Θ(E log(V)) if the graph is connected.
 
-*/
-
-#include <iostream>
-#include <queue>
-#include <vector>
-using namespace std;
-
-const int MAX_N = 101, INF = 1 << 28;
-int nodes, edges, a, b, weight, start, dest;
-int dist[MAX_N], pred[MAX_N];
-bool visit[MAX_N] = {0};
-vector< pair<int, int> > adj[MAX_N];
-
-int main() {
-    cin >> nodes >> edges;
-    for (int i = 0; i < edges; i++) {
-        cin >> a >> b >> weight;
-        adj[a].push_back(make_pair(b, weight));
-    }
-    cin >> start >> dest;
-
-    for (int i = 0; i < nodes; i++) {
-        dist[i] = INF;
-        pred[i] = -1;
-        visit[i] = false;
-    }
-    dist[start] = 0;
-    priority_queue< pair<int, int> > pq;
-    pq.push(make_pair(0, start));
-    while (!pq.empty()) {
-        a = pq.top().second;
-        pq.pop();
-        visit[a] = true;
-        for (int j = 0; j < adj[a].size(); j++) {
-            b = adj[a][j].first;
-            if (visit[b]) continue;
-            if (dist[b] > dist[a] + adj[a][j].second) {
-                dist[b] = dist[a] + adj[a][j].second;
-                pred[b] = a;
-                pq.push(make_pair(-dist[b], b));
-            }
-        }
-    }
-    cout << "The shortest distance from " << start;
-    cout << " to " << dest << " is " << dist[dest] << ".\n";
-
-    /* Use pred[] to backtrack and print the path */
-    int i = 0, j = dest, path[MAX_N];
-    while (pred[j] != -1) j = path[++i] = pred[j];
-    cout << "Take the path: ";
-    while (i > 0) cout << path[i--] << "->";
-    cout << dest << ".\n";
-    return 0;
-} 
-
-/*
-
 Implementation Notes: The graph is stored using an adjacency list.
 This implementation negates distances before adding them to the 
 priority queue, since the container is a max-heap by default. This
@@ -97,3 +40,55 @@ The shortest distance from 1 to 4 is 5.
 Take the path 1->2->3->4.
 
 */
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+const int MAX_N = 100, INF = 1 << 28;
+int nodes, edges, a, b, weight, start, dest;
+int dist[MAX_N+1], pred[MAX_N+1];
+bool visit[MAX_N+1] = {0};
+vector< pair<int, int> > adj[MAX_N+1];
+
+int main() {
+  cin >> nodes >> edges;
+  for (int i = 0; i < edges; i++) {
+    cin >> a >> b >> weight;
+    adj[a].push_back(make_pair(b, weight));
+  }
+  cin >> start >> dest;
+  for (int i = 0; i < nodes; i++) {
+    dist[i] = INF;
+    pred[i] = -1;
+    visit[i] = false;
+  }
+  dist[start] = 0;
+  priority_queue< pair<int, int> > pq;
+  pq.push(make_pair(0, start));
+  while (!pq.empty()) {
+    a = pq.top().second;
+    pq.pop();
+    visit[a] = true;
+    for (int j = 0; j < adj[a].size(); j++) {
+      b = adj[a][j].first;
+      if (visit[b]) continue;
+      if (dist[b] > dist[a] + adj[a][j].second) {
+        dist[b] = dist[a] + adj[a][j].second;
+        pred[b] = a;
+        pq.push(make_pair(-dist[b], b));
+      }
+    }
+  }
+  cout << "The shortest distance from " << start;
+  cout << " to " << dest << " is " << dist[dest] << ".\n";
+
+  /* Optional: Use pred[] to backtrack and print the path */
+  int i = 0, j = dest, path[MAX_N];
+  while (pred[j] != -1) j = path[++i] = pred[j];
+  cout << "Take the path: ";
+  while (i > 0) cout << path[i--] << "->";
+  cout << dest << ".\n";
+  return 0;
+} 
