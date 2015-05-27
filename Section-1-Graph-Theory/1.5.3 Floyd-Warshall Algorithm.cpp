670ebff69a79ea4e9e88bcf9d908d65368558d66
Alex Li <Alextrovert@gmail.com>
2015-05-27 19:25:32 -0400

    Update 1.5.3 Floyd-Warshall Algorithm.cpp

--- Section-1-Graph-Theory/1.5.3 Floyd-Warshall Algorithm.cpp
+++ Section-1-Graph-Theory/1.5.3 Floyd-Warshall Algorithm.cpp
@@ -8,55 +8,6 @@ of nodes. The input graph is stored using an adjacency matrix.
 
 Complexity: O(V^3) on the number of vertices.
 
-*/
-
-#include <iostream>
-using namespace std;
-
-const int MAX_N = 101, INF = 1<<28;
-int nodes, edges, a, b, weight, start, dest;
-int dist[MAX_N][MAX_N], next[MAX_N][MAX_N];
-
-void print_path(int i, int j) {
-    if (next[i][j] != -1) {
-        print_path(i, next[i][j]);
-        cout << next[i][j];
-        print_path(next[i][j], j);
-    } else cout << "->";
-}
-
-int main() {
-    cin >> nodes >> edges;
-    for (int i = 1; i <= nodes; i++)
-        for (int j = 1; j <= nodes; j++) {
-            dist[i][j] = (i == j) ? 0 : INF;
-            next[i][j] = -1;
-        }
-    for (int i = 0; i < edges; i++) {
-        cin >> a >> b >> weight;
-        dist[a][b] = weight;
-    }
-    cin >> start >> dest;
-    for (int k = 1; k <= nodes; k++)
-     for (int i = 1; i <= nodes; i++)
-      for (int j = 1; j <= nodes; j++)
-        if (dist[i][j] > dist[i][k] + dist[k][j]) {
-            dist[i][j] = dist[i][k] + dist[k][j];
-            next[i][j] = k;
-        }
-    cout << "The shortest path from " << start;
-    cout << " to " << dest << " is ";
-    cout << dist[start][dest] << ".\n";
-
-    /* Use next[][] to recursively print the path */
-    cout << "Take the path " << start;
-    print_path(start, dest);
-    cout << dest << ".\n";
-    return 0;
-}
-
-/*
-
 =~=~=~=~= Sample Input =~=~=~=~=
 3 3
 1 2 1
@@ -69,3 +20,48 @@ The shortest path from 1 to 3 is 3.
 Take the path 1->2->3. 
 
 */
+
+#include <iostream>
+using namespace std;
+
+const int MAXN = 100, INF = 1<<28;
+int nodes, edges, a, b, weight, start, dest;
+int dist[MAXN+1][MAXN+1], next[MAXN+1][MAXN+1];
+
+void print_path(int i, int j) {
+  if (next[i][j] != -1) {
+    print_path(i, next[i][j]);
+    cout << next[i][j];
+    print_path(next[i][j], j);
+  } else cout << "->";
+}
+
+int main() {
+  cin >> nodes >> edges;
+  for (int i = 1; i <= nodes; i++)
+    for (int j = 1; j <= nodes; j++) {
+      dist[i][j] = (i == j) ? 0 : INF;
+      next[i][j] = -1;
+    }
+  for (int i = 0; i < edges; i++) {
+    cin >> a >> b >> weight;
+    dist[a][b] = weight;
+  }
+  cin >> start >> dest;
+  for (int k = 1; k <= nodes; k++)
+   for (int i = 1; i <= nodes; i++)
+    for (int j = 1; j <= nodes; j++)
+      if (dist[i][j] > dist[i][k] + dist[k][j]) {
+          dist[i][j] = dist[i][k] + dist[k][j];
+          next[i][j] = k;
+      }
+  cout << "The shortest path from " << start;
+  cout << " to " << dest << " is ";
+  cout << dist[start][dest] << ".\n";
+
+  /* Use next[][] to recursively print the path */
+  cout << "Take the path " << start;
+  print_path(start, dest);
+  cout << dest << ".\n";
+  return 0;
+}
