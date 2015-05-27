f9615400483c941fddb6490252cbfd236feff780
Alex Li <Alextrovert@gmail.com>
2015-05-27 19:35:26 -0400

    Update 1.6.2 Prim's Algorithm.cpp

--- Section-1-Graph-Theory/1.6.2 Prim's Algorithm.cpp
+++ Section-1-Graph-Theory/1.6.2 Prim's Algorithm.cpp
@@ -9,64 +9,6 @@ simplest O(V^2) version of the algorithm, which uses looping and
 an adjacency matrix. If the priority queue is implemented as a more
 sophisticated Fibonacci heap, the complexity becomes O(E + V*log(V)).  
 
-*/
-
-#include <iostream>
-#include <queue>
-#include <vector>
-using namespace std;
-
-const int MAX_N = 101;
-int nodes, edges, a, b, weight, pred[MAX_N];
-bool visit[MAX_N] = {0};
-vector< pair<int, int> > adj[MAX_N];
- 
-int main() {
-    cin >> nodes >> edges;
-    for (int i = 0; i < edges; i++) {
-        cin >> a >> b >> weight;
-        adj[a].push_back(make_pair(b, weight));
-        adj[b].push_back(make_pair(a, weight));
-    }
-
-    for (int i = 1; i <= nodes; i++) {
-        pred[i] = -1;
-        visit[i] = false;
-    }
-    int start = 1; /* arbitrarily pick 1 as a starting node */
-    visit[start] = true;
-    priority_queue< pair<int, pair<int, int> > > pq;
-    for (int j = 0; j < adj[start].size(); j++)
-        pq.push(make_pair(-adj[start][j].second,
-                 make_pair(start, adj[start][j].first)));
-    int MSTnodes = 1, totalDistance = 0;
-    while (MSTnodes < nodes) {
-        if (pq.empty()) {
-            cout << "Error: Graph is not connected!\n";
-            return 0;
-        }
-        weight = -pq.top().first;
-        a = pq.top().second.first;
-        b = pq.top().second.second;
-        pq.pop();
-        if (visit[a] && !visit[b]) {
-            visit[b] = true;
-            MSTnodes++;
-            pred[b] = a;
-            totalDistance += weight;
-            for (int j = 0; j < adj[b].size(); j++)
-                pq.push(make_pair(-adj[b][j].second,
-                         make_pair(b, adj[b][j].first)));
-        }
-    }
-    for (int i = 2; i <= nodes; i++)
-        cout << i << "<->" << pred[i] << "\n";
-    cout << "Total distance: " << totalDistance << "\n";
-    return 0;
-} 
-
-/*
-
 =~=~=~=~= Sample Input =~=~=~=~=
 9 14
 1 2 4
@@ -95,7 +37,6 @@ int main() {
 9<->3
 Total distance: 37
 
-
 Implementation Notes:
 
 The input graph is stored in an adjacency list. Similar to the
@@ -114,3 +55,53 @@ positive constant added to all of the edge weights of the
 graph will not change the resulting spanning tree.
 
 */
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+const int MAXN = 100;
+int nodes, edges, a, b, weight, pred[MAXN+1];
+bool visit[MAXN+1] = {0};
+vector< pair<int, int> > adj[MAXN+1];
+ 
+int main() {
+  cin >> nodes >> edges;
+  for (int i = 0; i < edges; i++) {
+    cin >> a >> b >> weight;
+    adj[a].push_back(make_pair(b, weight));
+    adj[b].push_back(make_pair(a, weight));
+  }
+  for (int i = 1; i <= nodes; i++) pred[i] = -1;
+  int start = 1; /* arbitrarily pick 1 as a starting node */
+  visit[start] = true;
+  priority_queue< pair<int, pair<int, int> > > pq;
+  for (int j = 0; j < adj[start].size(); j++)
+    pq.push(make_pair(-adj[start][j].second,
+              make_pair(start, adj[start][j].first)));
+  int MSTnodes = 1, totalDistance = 0;
+  while (MSTnodes < nodes) {
+    if (pq.empty()) {
+      cout << "Error: Graph is not connected!\n";
+      return 0;
+    }
+    weight = -pq.top().first;
+    a = pq.top().second.first;
+    b = pq.top().second.second;
+    pq.pop();
+    if (visit[a] && !visit[b]) {
+      visit[b] = true;
+      MSTnodes++;
+      pred[b] = a;
+      totalDistance += weight;
+      for (int j = 0; j < adj[b].size(); j++)
+          pq.push(make_pair(-adj[b][j].second,
+                    make_pair(b, adj[b][j].first)));
+    }
+  }
+  for (int i = 2; i <= nodes; i++)
+    cout << i << "<->" << pred[i] << "\n";
+  cout << "Total distance: " << totalDistance << "\n";
+  return 0;
+} 
