aa00dc097b2eacae7ef5b016d3022a654094b50e
Alex Li <Alextrovert@gmail.com>
2015-05-27 23:52:15 -0400

    Update 3.2.1 Binary Indexed Tree (Simple - Point Update, Range Query).cpp

--- Section-3-Data-Structures/3.2.1 Binary Indexed Tree (Simple - Point Update, Range Query).cpp
+++ Section-3-Data-Structures/3.2.1 Binary Indexed Tree (Simple - Point Update, Range Query).cpp
@@ -15,29 +15,33 @@ functions are O(1).
 Space Complexity: O(N) storage and O(N) auxiliary on the number
 of elements in the array.
 
+=~=~=~=~= Sample Output =~=~=~=~=
+BIT values: 1 2 3 4 5
+Sum of range [1,3] is 6.
+
 */
 
 const int SIZE = 1000;
 int a[SIZE+1], bit[SIZE+1];
 
 int query_pre(int x) { //sum(a[1..x])
-    int ret = 0;
-    for (; x > 0; x -= x & -x) ret += bit[x];
-    return ret;
+  int ret = 0;
+  for (; x > 0; x -= x & -x) ret += bit[x];
+  return ret;
 }
 
 int query(int x, int y) { //sum(a[x..y])
-    return query_pre(y) - query_pre(x - 1);
+  return query_pre(y) - query_pre(x - 1);
 }
 
 int update_inc(int x, int v) { //a[x] += v
-    for (; x <= SIZE; x += x & -x) bit[x] += v;
+  for (; x <= SIZE; x += x & -x) bit[x] += v;
 }
 
 int update(int x, int v) { //a[x] = v
-    int inc = v - a[x];
-    a[x] = v;
-    update_inc(x, inc);
+  int inc = v - a[x];
+  a[x] = v;
+  update_inc(x, inc);
 }
 
 /*** Example Usage: ***/
@@ -46,19 +50,10 @@ int update(int x, int v) { //a[x] = v
 using namespace std;
 
 int main() {
-    for (int i = 1; i <= 5; i++) update(i, i);
-    cout << "BIT values: ";
-    for (int i = 1; i <= 5; i++)
-      cout << query(i, i) << " ";
-    cout << "\nSum of range [1,3] is ";
-    cout << query(1, 3) << ".\n";
-    return 0;
+  for (int i = 1; i <= 5; i++) update(i, i);
+  cout << "BIT values: ";
+  for (int i = 1; i <= 5; i++) cout << query(i, i) << " ";
+  cout << "\nSum of range [1,3] is ";
+  cout << query(1, 3) << ".\n";
+  return 0;
 } 
-
-/*
-
-=~=~=~=~= Sample Output =~=~=~=~=
-BIT values: 1 2 3 4 5
-Sum of range [1,3] is 6.
-
-*/
