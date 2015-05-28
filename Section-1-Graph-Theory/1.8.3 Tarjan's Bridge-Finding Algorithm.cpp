7a9d0f2f25f18e8d96eb75ad823959cd2df49bbf
Alex Li <Alextrovert@gmail.com>
2015-05-27 21:03:50 -0400

    Update 1.8.3 Tarjan's Bridge-Finding Algorithm.cpp

--- Section-1-Graph-Theory/1.8.3 Tarjan's Bridge-Finding Algorithm.cpp
+++ Section-1-Graph-Theory/1.8.3 Tarjan's Bridge-Finding Algorithm.cpp
@@ -8,52 +8,6 @@ An edge is a bridge iff if it is not contained in any cycle.
 
 Complexity: O(V + E) on the number of vertices and edges.
 
-*/
-
-#include <iostream>
-#include <vector>
-using namespace std;
-
-const int MAX_N = 101;
-int nodes, edges, a, b, counter, num[MAX_N], low[MAX_N];
-bool visit[MAX_N] = {0};
-vector<int> adj[MAX_N];
-
-void DFS(int a, int c) {
-    int b;
-    low[a] = num[a] = ++counter;
-    for (int j = 0; j < adj[a].size(); j++) {
-        if (num[b = adj[a][j]] == -1) {
-            DFS(b, a);
-            low[a] = min(low[a], low[b]);
-        } else if (b != c)
-            low[a] = min(low[a], num[b]);
-    }
-    for (int j = 0; j < adj[a].size(); j++)
-        if (low[b = adj[a][j]] > num[a])
-            cout << a+1 << "-" << b+1 << " is a bridge\n";
-}
-
-int main() {
-    cin >> nodes >> edges;
-    for (int i = 0; i < edges; i++) {
-        cin >> a >> b;
-        a--, b--;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-    for (int i = 0; i < nodes; i++) {
-        num[i] = low[i] = -1;
-        visit[i] = false;
-    }
-    counter = 0;
-    for (int i = 0; i < nodes; i++) 
-        if (num[i] == -1) DFS(i, -1);
-    return 0;
-}
-
-/*
-
 =~=~=~=~= Sample Input =~=~=~=~=
 8 6
 1 2
@@ -64,8 +18,51 @@ int main() {
 5 6
 
 =~=~=~=~= Sample Output =~=~=~=~=
-6-5 is a bridge.
-2-3 is a bridge.
-4-8 is a bridge.
+6-5
+2-3
+4-8
 
 */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAXN = 100;
+int nodes, edges, a, b, counter;
+int num[MAXN], low[MAXN];
+bool visit[MAXN] = {0};
+vector<int> adj[MAXN];
+
+void DFS(int a, int c) {
+  int b;
+  low[a] = num[a] = ++counter;
+  for (int j = 0; j < adj[a].size(); j++) {
+    if (num[b = adj[a][j]] == -1) {
+      DFS(b, a);
+      low[a] = min(low[a], low[b]);
+    } else if (b != c)
+      low[a] = min(low[a], num[b]);
+  }
+  for (int j = 0; j < adj[a].size(); j++)
+    if (low[adj[a][j]] > num[a])
+      cout << a+1 << "-" << adj[a][j]+1 << "\n";
+}
+
+int main() {
+  cin >> nodes >> edges;
+  for (int i = 0; i < edges; i++) {
+    cin >> a >> b;
+    a--, b--;
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+  }
+  for (int i = 0; i < nodes; i++) {
+    num[i] = low[i] = -1;
+    visit[i] = false;
+  }
+  counter = 0;
+  for (int i = 0; i < nodes; i++) 
+    if (num[i] == -1) DFS(i, -1);
+  return 0;
+}
