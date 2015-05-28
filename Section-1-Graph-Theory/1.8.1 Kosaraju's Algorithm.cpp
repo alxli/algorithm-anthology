6c0092634a8e06567228cd1467a251e1d6ff98fe
Alex Li <Alextrovert@gmail.com>
2015-05-27 20:53:45 -0400

    Update 1.8.1 Kosaraju's Algorithm.cpp

--- Section-1-Graph-Theory/1.8.1 Kosaraju's Algorithm.cpp
+++ Section-1-Graph-Theory/1.8.1 Kosaraju's Algorithm.cpp
@@ -17,60 +17,6 @@ a single DFS rather than two. Though they both have the same
 complexity, Tarjan’s algorithm is much more efficient in
 practice. However, Kosaraju’s algorithm is conceptually simpler.
 
-*/
-
-#include <algorithm> /* std::reverse() */
-#include <iostream>
-#include <vector>
-using namespace std;
-
-const int MAX_N = 101;
-int nodes, edges, a, b;
-bool visit[MAX_N] = {0};
-vector<int> adj[MAX_N], rev[MAX_N], order;
-vector< vector<int> > SCC;
-
-void DFS(vector<int> graph[], vector<int> &res, int i) {
-    visit[i] = true;
-    for (int j = 0; j < graph[i].size(); j++)
-        if (!visit[graph[i][j]])
-            DFS(graph, res, graph[i][j]);
-    res.push_back(i);
-}
-
-int main() {
-    cin >> nodes >> edges;
-    for (int i = 0; i < edges; i++) {
-        cin >> a >> b;    a--, b--;
-        adj[a].push_back(b);
-    }
-
-    for (int i = 0; i < nodes; i++) visit[i] = false;
-    for (int i = 0; i < nodes; i++)
-        if (!visit[i]) DFS(adj, order, i);
-    for (int i = 0; i < nodes; i++)
-        for (int j = 0; j < adj[i].size(); j++)
-            rev[adj[i][j]].push_back(i);
-    for (int i = 0; i < nodes; i++) visit[i] = false;
-    reverse(order.begin(), order.end());
-    for (int i = 0; i < order.size(); i++)
-        if (!visit[order[i]]) {
-            vector<int> component;
-            DFS(rev, component, order[i]);
-            SCC.push_back(component);
-        }
-
-    for (int i = 0; i < SCC.size(); i++) {
-        cout << "Component " << i + 1 << ":";
-        for (int j = 0; j < SCC[i].size(); j++)
-            cout << " " << (SCC[i][j] + 1);
-        cout << endl;
-    }
-    return 0;
-}
-
-/*
-
 =~=~=~=~= Sample Input =~=~=~=~=
 8 14
 1 2
@@ -94,3 +40,50 @@ Component 2: 8 4 3
 Component 3: 6 7
 
 */
+
+#include <algorithm> /* std::reverse() */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAXN = 100;
+int nodes, edges, a, b;
+bool visit[MAXN+1] = {0};
+vector<int> adj[MAXN+1], rev[MAXN+1], order;
+vector< vector<int> > SCC;
+
+void DFS(vector<int> graph[], vector<int> &res, int i) {
+  visit[i] = true;
+  for (int j = 0; j < graph[i].size(); j++)
+    if (!visit[graph[i][j]])
+      DFS(graph, res, graph[i][j]);
+  res.push_back(i);
+}
+
+int main() {
+  cin >> nodes >> edges;
+  for (int i = 0; i < edges; i++) {
+    cin >> a >> b;
+    adj[--a].push_back(--b);
+  }
+  for (int i = 0; i < nodes; i++)
+    if (!visit[i]) DFS(adj, order, i);
+  for (int i = 0; i < nodes; i++)
+    for (int j = 0; j < adj[i].size(); j++)
+      rev[adj[i][j]].push_back(i);
+  for (int i = 0; i < nodes; i++) visit[i] = false;
+  reverse(order.begin(), order.end());
+  for (int i = 0; i < order.size(); i++) {
+    if (visit[order[i]]) continue;
+    vector<int> component;
+    DFS(rev, component, order[i]);
+    SCC.push_back(component);
+  }
+  for (int i = 0; i < SCC.size(); i++) {
+    cout << "Component " << i + 1 << ":";
+    for (int j = 0; j < SCC[i].size(); j++)
+      cout << " " << (SCC[i][j] + 1);
+    cout << endl;
+  }
+  return 0;
+}
