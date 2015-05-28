a9d077c704eb4aca852cdebdfb72b39ae23481c7
Alex Li <Alextrovert@gmail.com>
2015-05-27 23:42:41 -0400

    Update 3.1.1 Disjoint Set Forest (Simple Version).cpp

--- Section-3-Data-Structures/3.1.1 Disjoint Set Forest (Simple Version).cpp
+++ Section-3-Data-Structures/3.1.1 Disjoint Set Forest (Simple Version).cpp
@@ -3,7 +3,8 @@
 3.1.1 - Disjoint Set Forest (Simple Version for Ints)
 
 Description: This data structure dynamically keeps track
-of items partitioned into non-overlapping sets.
+of items partitioned into non-overlapping sets (a disjoint
+set forest). It is also known as a union-find data structure.
 
 Time Complexity: Every function below is O(α(N))
 amortized on the number of items in the set, due to the
@@ -15,27 +16,49 @@ Space Complexity: O(N) total.
 
 */
 
-const int MAX_N = 1001;
-int num_sets = 0, root[MAX_N], rank[MAX_N];
+const int MAXN = 1001;
+int num_sets = 0, root[MAXN], rank[MAXN];
 
 int find_root(int x) {
-    if (root[x] != x) root[x] = find_root(root[x]);
-    return root[x];
+  if (root[x] != x) root[x] = find_root(root[x]);
+  return root[x];
 }
 
 void make_set(int x) {
-    root[x] = x; rank[x] = 0; num_sets++;
+  root[x] = x;
+  rank[x] = 0;
+  num_sets++;
 }
 
 bool is_united(int x, int y) {
-    return find_root(x) == find_root(y);
+  return find_root(x) == find_root(y);
 }
 
 void unite(int x, int y) {
-    int X = find_root(x), Y = find_root(y);
-    if (X == Y) return;
-    num_sets--;
-    if (rank[X] < rank[Y]) root[X] = Y;
-    else if (rank[X] > rank[Y]) root[Y] = X;
-    else rank[root[Y] = X]++;
+  int X = find_root(x), Y = find_root(y);
+  if (X == Y) return;
+  num_sets--;
+  if (rank[X] < rank[Y]) root[X] = Y;
+  else if (rank[X] > rank[Y]) root[Y] = X;
+  else rank[root[Y] = X]++;
+}
+
+/*** Example Usage ***/
+
+#include <cassert>
+#include <iostream>
+using namespace std;
+
+int main() {
+  for (char c = 'a'; c <= 'g'; c++) make_set(c);
+  unite('a', 'b');
+  unite('b', 'f');
+  unite('d', 'e');
+  unite('e', 'g');
+  assert(num_sets == 3);
+  assert(is_united('a', 'b'));
+  assert(!is_united('a', 'c'));
+  assert(!is_united('b', 'g'));
+  assert(is_united('d', 'g'));
+  return 0;
 }
