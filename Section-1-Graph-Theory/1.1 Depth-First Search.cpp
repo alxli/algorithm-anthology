de2567ede5019c7a603ff6a4bf79af31779f3ebb
Alex Li <Alextrovert@gmail.com>
2015-05-31 10:40:43 -0400

    Update 1.1 Depth-First Search.cpp

--- Section-1-Graph-Theory/1.1 Depth-First Search.cpp
+++ Section-1-Graph-Theory/1.1 Depth-First Search.cpp
@@ -14,22 +14,21 @@ without repetition. O(b^d) for implicit graphs with a branch
 factor of b, searched to depth d.
 
 =~=~=~=~= Sample Input =~=~=~=~=
-12 11 1
+12 11 0
+0 1
+0 6
+0 7
 1 2
-1 7
-1 8
+1 5
 2 3
-2 6
-3 4
-3 5
+2 4
+7 8
+7 11
 8 9
-8 12
-9 10
-9 11
+8 10
 
 =~=~=~=~= Sample Output =~=~=~=~=
-Nodes visited:
-1 2 3 4 5 6 7 8 9 10 11 12
+Nodes visited: 0 1 2 3 4 5 6 7 8 9 10 11
 
 */
 
@@ -39,14 +38,14 @@ using namespace std;
 
 const int MAXN = 100;
 int nodes, edges, start, a, b;
-bool visit[MAXN+1] = {0};
-vector<int> adj[MAXN+1];
+vector<bool> vis(MAXN);
+vector<int> adj[MAXN];
 
 void DFS(int n) {
-  visit[n] = true;
+  vis[n] = true;
   cout << " " << n;
   for (int j = 0; j < adj[n].size(); j++)
-    if (!visit[adj[n][j]]) DFS(adj[n][j]);
+    if (!vis[adj[n][j]]) DFS(adj[n][j]);
 }
 
 int main() {
@@ -55,7 +54,7 @@ int main() {
     cin >> a >> b;
     adj[a].push_back(b);
   }
-  cout << "Nodes visited:\n";
+  cout << "Nodes visited:";
   DFS(start);
   return 0;
 }
