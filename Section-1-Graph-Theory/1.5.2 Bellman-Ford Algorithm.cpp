5ff5f8f00974944ba70a9d03aa61926c2b4c6975
Alex Li <Alextrovert@gmail.com>
2015-05-31 11:07:58 -0400

    Update 1.5.2 Bellman-Ford Algorithm.cpp

--- Section-1-Graph-Theory/1.5.2 Bellman-Ford Algorithm.cpp
+++ Section-1-Graph-Theory/1.5.2 Bellman-Ford Algorithm.cpp
@@ -10,51 +10,50 @@ Complexity: O(V*E) on the number of vertices and edges, respectively.
 
 =~=~=~=~= Sample Input =~=~=~=~=
 3 3
-1 2 1
-2 3 2
-1 3 5
-1 3 
+0 1 1
+1 2 2
+0 2 5
+0 2
 
 =~=~=~=~= Sample Output =~=~=~=~=
-The shortest path from 1 to 3 is 3.
-Take the path 1->2->3. 
+The shortest distance from 0 to 2 is 3.
+Take the path: 0->1->2.
 
 */
 
 #include <iostream>
 using namespace std;
 
-const int MAXN=100, MAXE = MAXN*MAXN, INF=1<<28;
+const int MAXN = 100, INF = 0x3F3F3F3F;
 int nodes, edges, a, b, weight, start, dest;
-int E[MAXE][3], dist[MAXN+1], pred[MAXN+1];
+int E[MAXN*MAXN][3], dist[MAXN], pred[MAXN];
 
 int main() {
   cin >> nodes >> edges;
   for (int i = 0; i < edges; i++)
     cin >> E[i][0] >> E[i][1] >> E[i][2];
   cin >> start >> dest;
-  for (int i = 1; i <= nodes; i++) {
+  for (int i = 0; i < nodes; i++) {
     dist[i] = INF;
     pred[i] = -1;
   }
   dist[start] = 0;
-  for (int i = 1; i <= nodes; i++)
+  for (int i = 0; i < nodes; i++)
     for (int j = 0; j < edges; j++)
       if (dist[E[j][1]] > dist[E[j][0]] + E[j][2]) {
           dist[E[j][1]] = dist[E[j][0]] + E[j][2];
           pred[E[j][1]] = E[j][0];
       }
-  cout << "The shortest path from " << start;
-  cout << " to " << dest << " is ";
-  cout << dist[dest] << "." << endl;
+  cout << "The shortest distance from " << start;
+  cout << " to " << dest << " is " << dist[dest] << ".\n";
 
-  /* Optional: Report negative-weight cycles */
+  //Optional: Report negative-weight cycles
   for (int i = 0; i < edges; i++) 
     if (dist[E[i][0]] + E[i][2] < dist[E[i][1]])
       cout << "Negative-weight cycle detected!\n";
 
-  /* Use pred[] to backtrack and print the path */
-  int i = 0, j = dest, path[MAXN + 1];
+  //Optional: Use pred[] to backtrack and print the path
+  int i = 0, j = dest, path[MAXN];
   while (pred[j] != -1) j = path[++i] = pred[j];
   cout << "Take the path: ";
   while (i > 0) cout << path[i--] << "->";
