f3daf7d8821885c1f5af41c095542eef5e132d1d
Alex Li <Alextrovert@gmail.com>
2015-05-31 10:44:32 -0400

    Update 1.2 Breadth-First Search.cpp

--- Section-1-Graph-Theory/1.2 Breadth-First Search.cpp
+++ Section-1-Graph-Theory/1.2 Breadth-First Search.cpp
@@ -19,22 +19,21 @@ This will not work as intended with a priority queue,
 such as in Dijkstra’s algorithm.
 
 =~=~=~=~= Sample Input =~=~=~=~=
-12 11 1
-1 2
-1 3
+12 11 0
+0 1
+0 2
+0 3
 1 4
-2 5
-2 6
-4 7
+1 5
+3 6
+3 7
 4 8
-5 9
-5 10
-7 11
-7 12 
+4 9
+6 10
+6 11
 
 =~=~=~=~= Sample Output =~=~=~=~=
-Nodes visited:
-1 2 3 4 5 6 7 8 9 10 11 12
+Nodes visited: 0 1 2 3 4 5 6 7 8 9 10 11
 
 */
 
@@ -45,8 +44,8 @@ using namespace std;
 
 const int MAXN = 100;
 int nodes, edges, start, a, b;
-bool visit[MAXN+1] = {0};
-vector<int> adj[MAXN+1];
+vector<bool> vis(MAXN);
+vector<int> adj[MAXN];
 
 int main() {
   cin >> nodes >> edges >> start;
@@ -54,14 +53,14 @@ int main() {
     cin >> a >> b;
     adj[a].push_back(b);
   }
-  cout << "Nodes visited:\n";
+  cout << "Nodes visited:";
   queue<int> q;
   for (q.push(start); !q.empty(); q.pop()) {
     int n = q.front();
-    visit[n] = true;
+    vis[n] = true;
     cout << " " << n;
     for (int j = 0; j < adj[n].size(); j++)
-      if (!visit[adj[n][j]]) q.push(adj[n][j]);
+      if (!vis[adj[n][j]]) q.push(adj[n][j]);
   }
   return 0;
 }
