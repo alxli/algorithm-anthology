80c182a565487d4dad5e1b03971dbb9699fdff57
Alex Li <Alextrovert@gmail.com>
2015-06-06 23:38:05 -0400

    Create 3.3.6 RMQ: Sparse Table.cpp

---
+++ Section-3-Data-Structures/3.3.6 RMQ: Sparse Table.cpp
@@ -0,0 +1,62 @@
+/*
+
+3.3.4 - Range Minimum Query (Sparse Tables)
+
+Description: The static range minimum query problem can be solved
+using a sparse table data structure. The RMQ for sub arrays of
+length 2^k is pre-processed using dynamic programming with formula:
+
+dp[i][j] = dp[i][j-1], if A[dp[i][j-1]] <= A[dp[i+2^(j-1)-1][j-1]]
+           dp[i+2^(j-1)-1][j-1], otherwise
+
+where dp[i][j] is the index of the minimum value in the sub array
+starting at i having length 2^j.
+
+Time Complexity: O(N log N) for build() and O(1) for min_idx()
+Space Complexity: O(N log N) on the size of the array.
+
+Note: This implementation is 0-based, meaning that all
+indices from 0 to N - 1, inclusive, are valid.
+
+*/
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAXN = 100;
+
+vector<int> logtable, dp[MAXN];
+
+void build(int N, int a[]) {
+  logtable.resize(N + 1);
+  for (int i = 2; i <= N; i++)
+    logtable[i] = logtable[i >> 1] + 1;
+  for (int i = 0; i < N; i++) {
+    dp[i].resize(logtable[N] + 1);
+    dp[i][0] = i;
+  }
+  for (int k = 1; (1 << k) < N; k++) {
+    for (int i = 0; i + (1 << k) <= N; i++) {
+      int x = dp[i][k - 1];
+      int y = dp[i + (1 << k - 1)][k - 1];
+      dp[i][k] = a[x] <= a[y] ? x : y;
+    }
+  }
+}
+
+//returns index of min element in [lo, hi]
+int min_idx(int a[], int lo, int hi) {
+  int k = logtable[hi - lo];
+  int x = dp[lo][k];
+  int y = dp[hi - (1 << k) + 1][k];
+  return a[x] <= a[y] ? x : y;
+}
+
+int main() {
+  int a[] = {7, -10, 5, 20};
+  build(4, a);
+  cout << min_idx(a, 0, 3) << "\n"; //1
+  return 0;
+}
