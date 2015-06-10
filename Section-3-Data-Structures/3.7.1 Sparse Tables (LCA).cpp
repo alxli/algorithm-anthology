6cd8ad7a15b582e644c4d5ac3abf20a83b891bdf
Alex Li <Alextrovert@gmail.com>
2015-06-10 18:05:06 -0400

    Create 3.7.1 Sparse Tables (LCA).cpp

---
+++ Section-3-Data-Structures/3.7.1 Sparse Tables (LCA).cpp
@@ -0,0 +1,74 @@
+/*
+
+3.7.1 - Sparse Tables for Lowest Common Ancestor
+
+Description: Given an undirected graph that is a tree, the
+lowest common ancestor (LCA) of two nodes v and w is the
+lowest (i.e. deepest) node that has both v and w as descendants,
+where we define each node to be a descendant of itself (so if
+v has a direct connection from w, w is the lowest common
+ancestor). The following program uses sparse tables to solve
+the problem on an unchanging graph.
+
+Time Complexity: O(N log N) for build() and O(log N) for lca(),
+where N is the number of nodes in the tree.
+
+Space Complexity: O(N log N).
+
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAXN = 1000;
+int nodes, len, counter, tin[MAXN], tout[MAXN];
+vector<int> adj[MAXN], dp[MAXN];
+
+void dfs(int u, int p) {
+  tin[u] = counter++;
+  dp[u][0] = p;
+  for (int i = 1; i < len; i++)
+    dp[u][i] = dp[dp[u][i - 1]][i - 1];
+  for (int j = 0, v; j < adj[u].size(); j++)
+    if ((v = adj[u][j]) != p)
+      dfs(v, u);
+  tout[u] = counter++;
+}
+
+void build(int root) {
+  len = 1;
+  while ((1 << len) <= nodes) len++;
+  for (int i = 0; i < nodes; i++)
+    dp[i].resize(len);
+  dfs(root, root);
+}
+
+inline bool is_parent(int parent, int child) {
+  return tin[parent] <= tin[child] && tout[child] <= tout[parent];
+}
+
+int lca(int a, int b) {
+  if (is_parent(a, b)) return a;
+  if (is_parent(b, a)) return b;
+  for (int i = len - 1; i >= 0; i--)
+    if (!is_parent(dp[a][i], b))
+      a = dp[a][i];
+  return dp[a][0];
+}
+
+int main() {
+  nodes = 5;
+  adj[0].push_back(1);
+  adj[1].push_back(0);
+  adj[1].push_back(2);
+  adj[2].push_back(1);
+  adj[3].push_back(1);
+  adj[1].push_back(3);
+  adj[0].push_back(4);
+  adj[4].push_back(0);
+  build(0);
+  cout << lca(3, 2) << "\n"; //1
+  cout << lca(2, 4) << "\n"; //0
+  return 0;
+}
