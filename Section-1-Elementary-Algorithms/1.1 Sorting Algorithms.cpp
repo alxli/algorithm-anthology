d90901556de8115621942a00263464e5d57bb357
Alex Li <Alextrovert@gmail.com>
2015-06-19 14:06:55 -0400

    Update 1.1 Sorting Algorithms.cpp

--- Section-1-Elementary-Algorithms/1.1 Sorting Algorithms.cpp
+++ Section-1-Elementary-Algorithms/1.1 Sorting Algorithms.cpp
@@ -2,47 +2,46 @@
 
 1.1 - Sorting Algorithms
 
-The sorting functions in this section are included for novelty purposes,
-perhaps as demonstrations for ad hoc contest problems. With the exception
-of radix sort, there is generally no reason to use these sorts over C++’s
-built-in std::sort() and std::stable_sort() in an actual contest.
-
-The functions are implemented like std::sort(), taking in two
-RandomAccessIterators as the range to be sorted. To use with special
-comparators, either overload the < operator, or change every use of
-the < operator with array elements to a binary comparison function.
-E.g. to use quicksort() with a custom comparator,change "(*i < *lo)" to
-"(comp(*i, *lo))" wherever applicable
+The following functions behave just like the sorting functions
+found in the C++ standard library, taking RandomAccessIterators
+as the range to be sorted, and optionally a comparator.
 
 */
 
-#include <algorithm> /* std::swap(), std::(stable_)partition */
-#include <iterator>  /* std::iterator_traits<T> */
 
+#include <algorithm>  /* std::swap(), std::(stable_)partition */
+#include <functional> /* std::less<T>() */
+#include <iterator>   /* std::iterator_traits<T> */
 
 /*
 
 Quicksort
 
 Best: O(N*log(N))    Average: O(N*log(N))    Worst: O(N^2)
-  Auxiliary Space: O(log(N))               Stable?: No
+    Auxiliary Space: O(log(N))           Stable?: No
 
-Notes: The pivot value chosen here is always half way between lo
-and hi. Choosing random pivot values reduces the chances of
+Notes: The pivot value chosen here is always half way between
+lo and hi. Choosing random pivot values reduces the chances of
 encountering the worst case performance of O(N^2). Quicksort is
 faster in practice than other O(N*log(N)) algorithms.
 
 */
 
-template<class RAI> void quicksort(RAI lo, RAI hi) {
+template<class RAI, class Compare>
+void quicksort(RAI lo, RAI hi, Compare cmp) {
   if (lo + 1 >= hi) return;
   std::swap(*lo, *(lo + (hi - lo) / 2));
   RAI x = lo;
   for (RAI i = lo + 1; i < hi; i++)
-    if (*i < *lo) std::swap(*(++x), *i);
+    if (cmp(*i, *lo)) std::swap(*(++x), *i);
   std::swap(*lo, *x);
-  quicksort(lo, x);
-  quicksort(x + 1, hi);
+  quicksort(lo, x, cmp);
+  quicksort(x + 1, hi, cmp);
+}
+
+template<class RAI> void quicksort(RAI lo, RAI hi) {
+  typedef typename std::iterator_traits<RAI>::value_type T;
+  return quicksort(lo, hi, std::less<T>());
 }
 
 /*
@@ -52,30 +51,36 @@ Merge Sort
 Best: O(N*log(N))    Average: O(N*log(N))    Worst: O(N*log(N))
     Auxiliary Space: O(N)                Stable?: Yes
 
-Notes: Merge Sort has a better worse case complexity than
+Notes: Merge sort has a better worse case complexity than
 quicksort and is stable (i.e. it maintains the relative order
-of equivalent values after the sort). This is different from
-std::stable_sort() in that it will simply fail if extra memory
-is not available. Meanwhile, std::stable_sort() will fall back
-to a run time of O(N*log^2(N)) if out of memory.
+of equivalent values after the sort). The implementation below
+is different from std::stable_sort() in that it will simply fail
+if extra memory is not available. Meanwhile, std::stable_sort()
+will fall back to a run time of O(N*log^2(N)) if out of memory.
 
 */
 
-template<class RAI> void mergesort(RAI lo, RAI hi) {
+template<class RAI, class Compare>
+void mergesort(RAI lo, RAI hi, Compare cmp) {
+  typedef typename std::iterator_traits<RAI>::value_type T;
   if (lo >= hi - 1) return;
   RAI mid = lo + (hi - lo - 1) / 2, a = lo, c = mid + 1;
-  mergesort(lo, mid + 1);
-  mergesort(mid + 1, hi);
-  typedef typename std::iterator_traits<RAI>::value_type T;
-  T *buff = new T[hi - lo], *b = buff;
+  mergesort(lo, mid + 1, cmp);
+  mergesort(mid + 1, hi, cmp);
+  T buff[hi - lo], *b = buff;
   while (a <= mid && c < hi)
-    *(b++) = (*c < *a) ? *(c++) : *(a++);
+    *(b++) = cmp(*c, *a) ? *(c++) : *(a++);
   if (a > mid)
     for (RAI k = c; k < hi; k++) *(b++) = *k;
   else
     for (RAI k = a; k <= mid; k++) *(b++) = *k;
   for (int i = hi - lo - 1; i >= 0; i--)
     *(lo + i) = buff[i];
+}
+
+template<class RAI> void mergesort(RAI lo, RAI hi) {
+  typedef typename std::iterator_traits<RAI>::value_type T;
+  return mergesort(lo, hi, std::less<T>());
 }
 
 /*
@@ -85,13 +90,14 @@ Heap Sort
 Best: O(N*log(N))     Average: O(N*log(N))     Worst: O(N*log(N))
     Auxiliary Space: O(1)                 Stable?: No
 
-Notes: Heap Sort has a better worst case complexity than quicksort,
+Heap sort has a better worst case complexity than quicksort,
 but also a better space complexity than merge sort. On average,
-this will very likely run slower than a well implemented quicksort.
+this will likely run slower than a well implemented quicksort.
 
 */
 
-template<class RAI> void heapsort(RAI lo, RAI hi) {
+template<class RAI, class Compare>
+void heapsort(RAI lo, RAI hi, Compare cmp) {
   typename std::iterator_traits<RAI>::value_type t;
   RAI n = hi, i = (n - lo) / 2 + lo, parent, child;
   for (;;) {
@@ -99,15 +105,20 @@ template<class RAI> void heapsort(RAI lo, RAI hi) {
       if (--n == lo) return;
       t = *n, *n = *lo;
     } else t = *(--i);
-    parent = i, child = lo + (i - lo)*2 + 1;
+    parent = i, child = lo + 2 * (i - lo) + 1;
     while (child < n) {
-      if (child + 1 < n && *child < *(child + 1)) child++;
-      if (!(t < *child)) break;
+      if (child + 1 < n && cmp(*child, *(child + 1))) child++;
+      if (!cmp(t, *child)) break;
       *parent = *child, parent = child;
       child = lo + (parent - lo)*2 + 1;
     }
     *(lo + (parent - lo)) = t;
   }
+}
+
+template<class RAI> void heapsort(RAI lo, RAI hi) {
+  typedef typename std::iterator_traits<RAI>::value_type T;
+  return heapsort(lo, hi, std::less<T>());
 }
 
 /*
@@ -122,57 +133,66 @@ replace O(N*log(N)) sorts because it is so simple to memorize.
 
 */
 
-template<class RAI> void combsort(RAI lo, RAI hi) {
+template<class RAI, class Compare>
+void combsort(RAI lo, RAI hi, Compare cmp) {
   int gap = hi - lo, swapped = 1;
   while (gap > 1 || swapped) {
     if (gap > 1) gap = (int)((float)gap/1.3f);
     swapped = 0;
     for (RAI i = lo; i + gap < hi; i++)
-      if (*(i + gap) < *i) {
+      if (cmp(*(i + gap), *i)) {
         std::swap(*i, *(i + gap));
         swapped = 1;
       }
   }
 }
 
+template<class RAI> void combsort(RAI lo, RAI hi) {
+  typedef typename std::iterator_traits<RAI>::value_type T;
+  return combsort(lo, hi, std::less<T>());
+}
+
 /*
 
 Radix Sort (32-bit Signed Integers Only!)
 
-Worst: O(d*N), where d is the number of bits/digits in each of the N values.
-Since d is constant (32), one can say that radix sort runs in linear time.
+Worst: O(d*N), where d is the number of bits/digits in each
+of the N values. Since d is constant (32), one can say that
+radix sort runs in linear time.
 
 Auxiliary Space: O(d)
-Stable?: The LSD (least significant digit) radix sort is stable, but because
-std::stable_partition() is slower than the unstable std::partition(), the
-MSD (most significant digit) radix sort is much faster. Both are given here,
-with the MSD version implemented recursively.
 
-Notes: Radix sort usually employs a bucket sort or counting sort.
-Here, we take advantage of the partition functions found in the C++ library.
+Stable?: The LSD (least significant digit) radix sort is
+stable, but because std::stable_partition() is slower than
+the unstable std::partition(), the MSD (most significant
+digit) radix sort is much faster. Both are given here, with
+the MSD version implemented recursively.
+
+Notes: Radix sort usually employs a bucket sort or counting
+sort. Here, we take advantage of the partition functions
+found in the C++ library.
 
 */
 
 struct radix_comp { //UnaryPredicate for std::partition
   const int p; //bit position [0..31] to examine
-  radix_comp(int offset) : p(offset) { }
-  bool operator()(int value) const {
-    return (p == 31) ? (value < 0) : !(value & (1 << p));
+  radix_comp(int offset): p(offset) {}
+  bool operator() (int v) const {
+    return (p == 31) ? (v < 0) : !(v & (1 << p));
   }
 };
 
-void lsd_radix_sort(int *lo, int *hi) {
-  for (int lsb = 0; lsb < 32; ++lsb)
+void lsd_radix_sort(int * lo, int * hi) {
+  for (int lsb = 0; lsb < 32; lsb++)
     std::stable_partition(lo, hi, radix_comp(lsb));
 }
 
-void msd_radix_sort(int *lo, int *hi, int msb = 31) {
+void msd_radix_sort(int * lo, int * hi, int msb = 31) {
   if (lo == hi || msb < 0) return;
-  int *mid = std::partition(lo, hi, radix_comp(msb--));
+  int * mid = std::partition(lo, hi, radix_comp(msb--));
   msd_radix_sort(lo, mid, msb);
   msd_radix_sort(mid, hi, msb);
 }
-
 
 /*** Example Usage ***/
 
@@ -180,34 +200,64 @@ void msd_radix_sort(int *lo, int *hi, int msb = 31) {
 #include <vector>
 using namespace std;
 
+bool compare_as_ints(double i, double j) {
+  return (int)i < (int)j;
+}
+
 template<class T> void print_range(T lo, T hi) {
-  while (lo != hi) cout << " " << *(lo++);
+  while (lo != hi) cout << *(lo++) << " ";
   cout << "\n";
 }
 
 int main () {
-  int a1[] = {32, 71, 12, 45, 26, 80, 53, 33};
-  quicksort(a1, a1 + 8);
-  print_range(a1, a1 + 8);
+  {
+    int a[] = {32, 71, 12, 45, 26, 80, 53, 33};
+    quicksort(a, a + 8);
+    print_range(a, a + 8);
+  }
 
-  int a2[] = {32, 71, 12, 45, 26, 80, 53, 33};
-  vector<int> v(a2, a2 + 8);
-  quicksort(v.begin(), v.end()); //STL works too
-  print_range(v.begin(), v.end());
+  {
+    int a[] = {32, 71, 12, 45, 26, 80, 53, 33};
+    vector<int> v(a, a + 8);
+    quicksort(v.begin(), v.end()); //STL works too
+    print_range(v.begin(), v.end());
+  }
 
-  int a3[] = {32, 71, 12, 45, 26, 80, 53, 33};
-  v = vector<int>(a3, a3 + 8);
-  heapsort(v.rbegin(), v.rend()); //rvs iterators OK
-  print_range(v.begin(), v.end());
+  {
+    int a[] = {32, 71, 12, 45, 26, 80, 53, 33};
+    vector<int> v(a, a + 8);
+    heapsort(v.rbegin(), v.rend()); //rvs iterators OK
+    print_range(v.begin(), v.end());
+  }
 
-  double a4[] = {1.1, -5.0, 6.23, 4.123, 155.2};
-  vector<double> v2(a4, a4 + 5); //doubles OK too!
-  combsort(v2.begin(), v2.end());
-  print_range(v2.begin(), v2.end());
+  {
+    double a[] = {1.1, -5.0, 6.23, 4.123, 155.2};
+    vector<double> v(a, a + 5); //doubles OK too!
+    combsort(v.begin(), v.end());
+    print_range(v.begin(), v.end());
+  }
 
-  //radix sort only works on 32-bit signed ints
-  int a5[] = {1, 886, 6, -50, 7, -11, 123, 4};
-  msd_radix_sort(a5, a5 + 8);
-  print_range(a5, a5 + 8);
+  {
+    //radix sort only works on 32-bit signed ints
+    int a[] = {1, 886, 6, -50, 7, -11, 123, 4};
+    msd_radix_sort(a, a + 8);
+    print_range(a, a + 8);
+  }
+
+  //example from: cplusplus.com/reference/algorithm/stable_sort/
+  double a[] = {3.14, 1.41, 2.72, 4.67, 1.73, 1.32, 1.62, 2.58};
+  {
+    vector<double> v(a, a + 8);
+    cout << "mergesort() with default comparison: ";
+    mergesort(v.begin(), v.end());
+    print_range(v.begin(), v.end());
+  }
+  {
+    vector<double> v(a, a + 8);
+    cout << "mergesort() with 'compare_as_ints' : ";
+    mergesort(v.begin(), v.end(), compare_as_ints);
+    print_range(v.begin(), v.end());
+  }
+
   return 0;
 }
