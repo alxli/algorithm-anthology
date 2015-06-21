4f6fde1ee44a17bd354f9dd17fd042bd24867beb
Alex Li <Alextrovert@gmail.com>
2015-06-20 21:29:29 -0400

    Update 3.3.6 Sparse Table (RMQ).cpp

--- Section-3-Data-Structures/3.3.6 Sparse Table (RMQ).cpp
+++ Section-3-Data-Structures/3.3.6 Sparse Table (RMQ).cpp
@@ -1,61 +1,53 @@
 /*
 
-3.7.1 - Sparse Tables for Lowest Common Ancestor
+3.3.4 - Range Minimum Query using a Sparse Table
 
-Description: Given a rooted tree, the lowest common ancestor (LCA)
-of two nodes v and w is the lowest (i.e. deepest) node that has
-both v and w as descendants, where we define each node to be a
-descendant of itself (so if v has a direct connection from w, w
-is the lowest common ancestor). This problem can be solved using
-sparse tables. A 2D table of size N by log N is computed using:
+Description: The static range minimum query problem can be solved
+using a sparse table data structure. The RMQ for sub arrays of
+length 2^k is pre-processed using dynamic programming with formula:
 
-dp[i][j] = parent[i],               if j = 0
-           dp[dp[i][j - 1]][i - 1], if j > 0
+dp[i][j] = dp[i][j-1], if A[dp[i][j-1]] <= A[dp[i+2^(j-1)-1][j-1]]
+           dp[i+2^(j-1)-1][j-1], otherwise
 
-where dp[i][j] is the (2^j)-th ancestor of i.
+where dp[i][j] is the index of the minimum value in the sub array
+starting at i having length 2^j.
 
-Time Complexity: O(N log N) for build() and O(log N) for lca(),
-where N is the number of nodes in the tree.
+Time Complexity: O(N log N) for build() and O(1) for min_idx()
+Space Complexity: O(N log N) on the size of the array.
 
-Space Complexity: O(N log N).
+Note: This implementation is 0-based, meaning that all
+indices from 0 to N - 1, inclusive, are valid.
 
 */
 
 #include <vector>
 
-const int MAXN = 1000;
-int len, timer, tin[MAXN], tout[MAXN];
-std::vector<int> adj[MAXN], dp[MAXN];
+const int MAXN = 100;
+std::vector<int> logtable, dp[MAXN];
 
-void dfs(int u, int p) {
-  tin[u] = timer++;
-  dp[u][0] = p;
-  for (int i = 1; i < len; i++)
-    dp[u][i] = dp[dp[u][i - 1]][i - 1];
-  for (int j = 0; j < adj[u].size(); j++)
-    if (adj[u][j] != p) dfs(adj[u][j], u);
-  tout[u] = timer++;
+void build(int n, int a[]) {
+  logtable.resize(n + 1);
+  for (int i = 2; i <= n; i++)
+    logtable[i] = logtable[i >> 1] + 1;
+  for (int i = 0; i < n; i++) {
+    dp[i].resize(logtable[n] + 1);
+    dp[i][0] = i;
+  }
+  for (int k = 1; (1 << k) < n; k++) {
+    for (int i = 0; i + (1 << k) <= n; i++) {
+      int x = dp[i][k - 1];
+      int y = dp[i + (1 << k - 1)][k - 1];
+      dp[i][k] = a[x] <= a[y] ? x : y;
+    }
+  }
 }
 
-void build(int nodes, int root) {
-  len = 1;
-  while ((1 << len) <= nodes) len++;
-  for (int i = 0; i < nodes; i++)
-    dp[i] = std::vector<int>(len);
-  timer = 0;
-  dfs(root, root);
-}
-
-inline bool is_parent(int p, int c) {
-  return tin[p] <= tin[c] && tout[c] <= tout[p];
-}
-
-int lca(int a, int b) {
-  if (is_parent(a, b)) return a;
-  if (is_parent(b, a)) return b;
-  for (int i = len - 1; i >= 0; i--)
-    if (!is_parent(dp[a][i], b)) a = dp[a][i];
-  return dp[a][0];
+//returns index of min element in [lo, hi]
+int min_idx(int a[], int lo, int hi) {
+  int k = logtable[hi - lo];
+  int x = dp[lo][k];
+  int y = dp[hi - (1 << k) + 1][k];
+  return a[x] <= a[y] ? x : y;
 }
 
 /*** Example Usage ***/
@@ -64,16 +56,8 @@ int lca(int a, int b) {
 using namespace std;
 
 int main() {
-  adj[0].push_back(1);
-  adj[1].push_back(0);
-  adj[1].push_back(2);
-  adj[2].push_back(1);
-  adj[3].push_back(1);
-  adj[1].push_back(3);
-  adj[0].push_back(4);
-  adj[4].push_back(0);
-  build(5, 0);
-  cout << lca(3, 2) << "\n"; //1
-  cout << lca(2, 4) << "\n"; //0
+  int a[] = {7, -10, 5, 20};
+  build(4, a);
+  cout << min_idx(a, 0, 3) << "\n"; //1
   return 0;
 }
