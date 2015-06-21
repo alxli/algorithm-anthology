1d5a719d2d22944c916897d724c300a6f8a6ac3c
Alex Li <Alextrovert@gmail.com>
2015-06-20 21:17:59 -0400

    Update 3.5.4 Hashmap (Chaining).cpp

--- Section-3-Data-Structures/3.5.4 Hashmap (Chaining).cpp
+++ Section-3-Data-Structures/3.5.4 Hashmap (Chaining).cpp
@@ -107,8 +107,8 @@ struct class_hash {
   //Jenkins's 64-bit hash
   unsigned int operator () (unsigned long long key) {
     key += ~(key << 32); key ^= (key >> 22);
-    key += ~(key << 13); key ^= (key >> 8);
-    key +=  (key << 3);  key ^= (key >> 15);
+    key += ~(key << 13); key ^= (key >>  8);
+    key +=  (key <<  3); key ^= (key >> 15);
     key += ~(key << 27); key ^= (key >> 31);
     return key;
   }
