0b1183817392ec16285e19620c93b38f8e2c95ff
Alex Li <Alextrovert@gmail.com>
2015-09-06 22:39:42 -0400

    More goodies

--- Section-6-Strings (IN PROGRESS)/6.1 Strings Toolbox.cpp
+++ Section-6-Strings (IN PROGRESS)/6.1 Strings Toolbox.cpp
@@ -1,9 +1,11 @@
 /*
 
-6.1 - String Toolbox
+6.1 - Strings Toolbox
 
 Useful or trivial string operations. These functions are not particularly
-algorithmics, and are simply naive implementations using C++ features.
+algorithmic. They are typically naive implementations using C++ features.
+They depend on many features of the C++ <string> library, which tend to
+have an unspecified complexity. They may not be optimally efficient.
 
 */
 
@@ -14,8 +16,7 @@ algorithmics, and are simply naive implementations using C++ features.
 
 //integer to string conversion and vice versa using C++ features
 
-//itoa() is a nonstandard function in the C standard library (see below)
-//in addition, a standard std::to_string is introduced in C++0x
+//note that a similar std::to_string is introduced in C++0x
 template<class Int>
 std::string to_string(const Int & i) {
   std::ostringstream oss;
@@ -23,12 +24,90 @@ std::string to_string(const Int & i) {
   return oss.str();
 }
 
-//like atoi except during exceptional cases like overflows
+//like atoi, except during special cases like overflows
 int to_int(const std::string & s) {
   std::istringstream iss(s);
   int res;
   if (!(iss >> res)) /* complain */;
   return res;
+}
+
+/*
+
+itoa implementation (fast)
+documentation: http://www.cplusplus.com/reference/cstdlib/itoa/
+taken from: http://www.jb.man.ac.uk/~slowe/cpp/itoa.html
+
+*/
+
+char* itoa(int value, char * str, int base = 10) {
+  if (base < 2 || base > 36) {
+    *str = '\0';
+    return str;
+  }
+  char *ptr = str, *ptr1 = str, tmp_c;
+  int tmp_v;
+  do {
+    tmp_v = value;
+    value /= base;
+    *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789"
+             "abcdefghijklmnopqrstuvwxyz"[35 + (tmp_v - value * base)];
+  } while (value);
+  if (tmp_v < 0) *ptr++ = '-';
+  for (*ptr-- = '\0'; ptr1 < ptr; *ptr1++ = tmp_c) {
+    tmp_c = *ptr;
+    *ptr-- = *ptr1;
+  }
+  return str;
+}
+
+/*
+
+Trimming functions (in place). Given a string and optionally a series
+of characters to be considered for trimming, trims the string's ends
+(left, right, or both) and returns the string. Note that the ORIGINAL
+string is trimmed as it's passed by reference, despite the original
+reference being returned for convenience.
+
+*/
+
+std::string& ltrim(std::string & s, const std::string & delim = " \n\t\v\f\r") {
+  unsigned int pos = s.find_first_not_of(delim);
+  if (pos != std::string::npos) s.erase(0, pos);
+  return s;
+}
+
+std::string& rtrim(std::string & s, const std::string & delim = " \n\t\v\f\r") {
+  unsigned int pos = s.find_last_not_of(delim);
+  if (pos != std::string::npos) s.erase(pos);
+  return s;
+}
+
+std::string& trim(std::string & s, const std::string & delim = " \n\t\v\f\r") {
+  return ltrim(rtrim(s));
+}
+
+/*
+
+Returns a copy of the string s with all occurrences of the given
+string search replaced with the given string replace.
+
+Time Complexity: Unspecified, but proportional to the number of times
+the search string occurs and the complexity of std::string::replace,
+which is unspecified.
+
+*/
+
+std::string replace(std::string s,
+                    const std::string & search,
+                    const std::string & replace) {
+  if (search.empty()) return s;
+  unsigned int pos = 0;
+  while ((pos = s.find(search, pos)) != std::string::npos) {
+    s.replace(pos, search.length(), replace);
+    pos += replace.length();
+  }
+  return s;
 }
 
 /*
@@ -100,6 +179,7 @@ std::vector<std::string> explode(const std::string & s,
 /*** Example Usage ***/
 
 #include <cassert>
+#include <cstdio>
 #include <iostream>
 using namespace std;
 
@@ -113,6 +193,16 @@ void print(const vector<string> & v) {
 int main() {
   assert(to_string(123) + "4" == "1234");
   assert(to_int("1234") == 1234);
+  char buffer[50];
+  assert(string(itoa(1750, buffer, 10)) == "1750");
+  assert(string(itoa(1750, buffer, 16)) == "6d6");
+  assert(string(itoa(1750, buffer, 2)) == "11011010110");
+
+  string s("   abc \n");
+  string t = s;
+  assert(ltrim(s) == "abc \n");
+  assert(rtrim(s) == trim(t));
+  assert(replace("abcdabba", "ab", "00") == "00cd00ba");
 
   vector<string> tokens;
 
@@ -127,6 +217,5 @@ int main() {
   tokens = explode("a..b.cde....f", "..");
   cout << "explode: ";
   print(tokens); //["a", ".b.cde", "", ".f"]
-
   return 0;
 }
