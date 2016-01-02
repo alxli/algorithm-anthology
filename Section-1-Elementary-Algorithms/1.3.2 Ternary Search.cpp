c05123b49a96e91aec47ffefda2518d17fb2bdc4
Alex Li <Alextrovert@gmail.com>
2016-01-01 19:23:16 -0500

    Update

--- Section-1-Elementary-Algorithms/1.3.2 Ternary Search.cpp
+++ Section-1-Elementary-Algorithms/1.3.2 Ternary Search.cpp
@@ -2,65 +2,77 @@
 
 1.3.2 - Ternary Search
 
-Given a unimodal function f(x), find its maximum or minimum.
+Given a unimodal function f(x), find its maximum or minimum point to a
+an arbitrarily specified absolute error. A maximum unimodal function
+f(x) with domain [lo, hi] means that there must exist a number x such
+that for all pairs a, b in [lo, x] with a < b, f(a) < f(b) and that
+for all pairs c, d in [x, hi] with c < d, f(c) > f(d). For a minimum
+unimodal function, f(a) > f(b) and f(c) < f(d) will instead hold.
 
-Assume we are looking for a maximum of f(x) and that we know
-the maximum lies somewhere between A and B. For the algorithm
-to be applicable, there must be some value x such that:
->>> for all a,b with A <= a < b <= x, we have f(a) < f(b); and
->>> for all a,b with x <= a < b <= B, we have f(a) > f(b).
+In other words, for ternary_search_min() to work on a function f, the
+graph of f must resemble a "valley" with a single point as its minimum
+and no flat regions on its left side nor right side. Conversely, for
+ternary_search_max() to work, the graph of f must look like a "hill"
+with a single point as its maximum.
 
-In other words, for the graph of f(x) must look like a "valley"
-with a single point at its trough (lowest point) and no flat
-regions on the left side or right side for a ternary search to
-be able to find its minimum point. Similarly the graph of f(x)
-must look like a "hill" with a single peak for a ternary search
-to able able to find its maximum point.
+Time Complexity: At most O(log n) calls to f, where n is the distance
+between lo and hi divided by the desired absolute error (epsilon).
 
-The answer for both functions will be really close to the peak
-or trough by no further than +/-epsilon (0.00000001 by default).
+Space Complexity: O(1) auxiliary.
 
 */
 
 template<class UnimodalFunction>
-double ternary_search_min(double lo, double hi,
-                          UnimodalFunction f, double eps = 1e-8) {
+double ternary_search_min(double lo, double hi, UnimodalFunction f,
+                          double eps = 1e-9) {
   double lthird, hthird;
   while (hi - lo > eps) {
     lthird = lo + (hi - lo) / 3;
     hthird = hi - (hi - lo) / 3;
-    if (f(lthird) < f(hthird)) hi = hthird;
-    else lo = lthird;
+    if (f(lthird) < f(hthird))
+      hi = hthird;
+    else
+      lo = lthird;
   }
   return lo;
 }
 
 template<class UnimodalFunction>
-double ternary_search_max(double lo, double hi,
-                          UnimodalFunction f, double eps = 1e-8) {
+double ternary_search_max(double lo, double hi, UnimodalFunction f,
+                          double eps = 1e-9) {
   double lthird, hthird;
   while (hi - lo > eps) {
     lthird = lo + (hi - lo) / 3;
     hthird = hi - (hi - lo) / 3;
-    if (f(lthird) < f(hthird)) lo = lthird;
-    else hi = hthird;
+    if (f(lthird) < f(hthird))
+      lo = lthird;
+    else
+      hi = hthird;
   }
-  return lo;
+  return hi;
 }
 
 /*** Example Usage ***/
 
-#include <iostream>
-using namespace std;
+#include <cmath>
+#include <cassert>
 
 //parabola opening up with vertex at (-2, -24)
-double f1(double x) { return 3 * x * x + 12 * x - 12; }
+double f1(double x) {
+  return 3*x*x + 12*x - 12;
+}
 
-//parabola opening down with vertex at (0.105, 88.1)
-double f2(double x) { return -5.7 * x * x + 1.2 * x + 88; }
+//parabola opening down with vertex at (2/19, 8366/95)
+double f2(double x) {
+  return -5.7*x*x + 1.2*x + 88;
+}
+
+bool eq(double a, double b) {
+  return fabs(a - b) < 1e-9;
+}
 
 int main() {
-  cout << ternary_search_min(-99, 99, f1) << endl; //-2
-  cout << ternary_search_max(-99, 99, f2) << endl; //0.105263
+  assert(eq(ternary_search_min(-1000, 1000, f1), -2));
+  assert(eq(ternary_search_max(-1000, 1000, f2), 2.0/19));
   return 0;
 }
