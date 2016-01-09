3a55adf40d2c78d2cfb77d1be5b314cfe3cc6e23
Alex Li <Alextrovert@gmail.com>
2016-01-08 23:58:56 -0500

    Updates

--- Section-2-Graph-Theory/2.1.4 Unweighted Tree Centers (DFS).cpp
+++ Section-2-Graph-Theory/2.1.4 Unweighted Tree Centers (DFS).cpp
@@ -1,52 +1,42 @@
 /*
 
-The following applies to unweighted, undirected trees only.
+An unweighted tree possesses a center, centroid, and diameter. The
+following functions apply to a global, prepopulated adjacency matrix
+adj which satisfies the precondition that for every node v in adj[u],
+the node u also exists in adj[v]. All node indices must be between 0
+and the number of nodes minus 1 as given in the function arguments.
 
-find_centers(): Returns 1 or 2 tree centers. The center
-(or Jordan center) of a graph is the set of all vertices of
-minimum eccentricity, that is, the set of all vertices A
-where the max distance d(A,B) to other vertices B is minimal.
+find_centers() returns one or two tree centers. The center (or Jordan
+center) of a graph is the set of all nodes with minimum eccentricity,
+that is, the set of all nodes where the max distance to all other nodes
+is minimal.
 
-find_centroid(): Returns a vertex where all of its subtrees
-have size <= N/2, where N is the number of nodes in the tree.
+find_centroid() returns a node where all of its subtrees have a size
+less than or equal to n/2, where n is the number of nodes in the tree.
 
-diameter(): The diameter of a tree is the greatest distance
-d(A,B) between any two of the nodes in the tree.
+diameter() returns the greatest distance between any two nodes in the
+tree, using the famous double DFS technique.
 
-Complexity: All three functions are O(V) on the number of
-vertices in the tree.
-
-=~=~=~=~= Sample Input =~=~=~=~=
-6
-0 1
-1 2
-1 4
-3 4
-4 5
-
-=~=~=~=~= Sample Output =~=~=~=~=
-Center(s): 1 4
-Centroid: 4
-Diameter: 3
+Time Complexity: O(n) on the number of nodes in the tree.
+Space Complexity: O(n) auxiliary on the number of nodes in the tree.
 
 */
 
-#include <iostream>
 #include <vector>
-using namespace std;
+#include <utility> /* std::pair */
 
 const int MAXN = 100;
-vector<int> adj[MAXN];
+std::vector<int> adj[MAXN];
 
-vector<int> find_centers(int n) {
-  vector<int> leaves, degree(n);
-  for (int i = 0; i < n; i++) {
+std::vector<int> find_centers(int nodes) {
+  std::vector<int> leaves, degree(nodes);
+  for (int i = 0; i < nodes; i++) {
     degree[i] = adj[i].size();
     if (degree[i] <= 1) leaves.push_back(i);
   }
   int removed = leaves.size();
-  while (removed < n) {
-    vector<int> nleaves;
+  while (removed < nodes) {
+    std::vector<int> nleaves;
     for (int i = 0; i < (int)leaves.size(); i++) {
       int u = leaves[i];
       for (int j = 0; j < (int)adj[u].size(); j++) {
@@ -61,23 +51,23 @@ vector<int> find_centers(int n) {
   return leaves;
 }
 
-int find_centroid(int n, int u = 0, int p = -1) {
+int find_centroid(int nodes, int u = 0, int p = -1) {
   int cnt = 1, v;
   bool good_center = true;
   for (int j = 0; j < (int)adj[u].size(); j++) {
     if ((v = adj[u][j]) == p) continue;
-    int res = find_centroid(n, v, u);
+    int res = find_centroid(nodes, v, u);
     if (res >= 0) return res;
     int size = -res;
-    good_center &= (size <= n / 2);
+    good_center &= (size <= nodes / 2);
     cnt += size;
   }
-  good_center &= (n - cnt <= n / 2);
+  good_center &= (nodes - cnt <= nodes / 2);
   return good_center ? u : -cnt;
 }
 
-pair<int, int> dfs(int u, int p, int depth) {
-  pair<int, int> res = make_pair(depth, u);
+std::pair<int, int> dfs(int u, int p, int depth) {
+  std::pair<int, int> res = std::make_pair(depth, u);
   for (int j = 0; j < (int)adj[u].size(); j++)
     if (adj[u][j] != p)
       res = max(res, dfs(adj[u][j], u, depth + 1));
@@ -89,19 +79,26 @@ int diameter() {
   return dfs(furthest_vertex, -1, 0).first;
 }
 
+/*** Example Usage ***/
+
+#include <cassert>
+using namespace std;
+
 int main() {
-  int nodes, u, v;
-  cin >> nodes;
-  for (int i = 0; i < nodes - 1; i++) {
-    cin >> u >> v;
-    adj[u].push_back(v);
-    adj[v].push_back(u);
-  }
+  int nodes = 6;
+  adj[0].push_back(1);
+  adj[1].push_back(0);
+  adj[1].push_back(2);
+  adj[2].push_back(1);
+  adj[1].push_back(4);
+  adj[4].push_back(1);
+  adj[3].push_back(4);
+  adj[4].push_back(3);
+  adj[4].push_back(5);
+  adj[5].push_back(4);
   vector<int> centers = find_centers(nodes);
-  cout << "Center(s):";
-  for (int i = 0; i < (int)centers.size(); i++)
-    cout << " " << centers[i];
-  cout << "\nCentroid: " << find_centroid(nodes);
-  cout << "\nDiameter: " << diameter() << "\n";
+  assert(centers.size() == 2 && centers[0] == 1 && centers[1] == 4);
+  assert(find_centroid(nodes) == 4);
+  assert(diameter() == 3);
   return 0;
 }
