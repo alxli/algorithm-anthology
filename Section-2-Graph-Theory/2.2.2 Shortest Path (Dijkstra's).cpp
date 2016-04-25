26f27a57d943430f5cc6a36982e0a0e86cadbd6d
Alex Li <Alextrovert@gmail.com>
2016-04-24 20:43:51 -0400

    Updates

--- Section-2-Graph-Theory/2.2.2 Shortest Path (Dijkstra's).cpp
+++ Section-2-Graph-Theory/2.2.2 Shortest Path (Dijkstra's).cpp
@@ -1,46 +1,31 @@
 /*
 
-Description: Given a directed graph with positive weights only, find
-the shortest distance to all nodes from a single starting node.
+Given an weighted, directed graph with nonnegative weights only,
+traverse to every connected node and determine the shortest distance to
+each. Optionally, output the shortest path to a specific destination
+node using the predecessor array precomputed during the search.
 
-Implementation Notes: The graph is stored using an adjacency list.
-This implementation negates distances before adding them to the
-priority queue, since the container is a max-heap by default. This
-method is suggested in contests because it is easier than defining
-special comparators. An alternative would be declaring the queue
-with template parameters (clearly, this way is very verbose and ugly):
+This implementation negates node distances before adding them to the
+priority_queue, since the standard library container is a max-heap by
+default. This method is recommended over defining special comparators.
+An alternative would be declaring the priority_queue with the following
+template arguments (#include <functional> to access std::greater):
   priority_queue< pair<int, int>, vector<pair<int, int> >,
                   greater<pair<int, int> > > pq;
-If only the path between a single pair of nodes is needed, for speed,
-we may break out of the loop as soon as the destination is reached
-by inserting the line "if (a == dest) break;" after the line "pq.pop();"
 
-Complexity: This version uses an adjacency list and priority queue
-(internally a binary heap) and has a complexity of O((E+V) log V) =
-O(E log V). The priority queue and adjacency list improves the
-simplest O(V^2) version of the algorithm, which uses looping and
-an adjacency matrix. If the priority queue is implemented as a more
-sophisticated Fibonacci heap, the complexity becomes O(E + V log V).
+Dijkstra's algorithm may be easily modified to support negative edge
+weights by allowing nodes to be re-visited (i.e. removing the first
+if-statement in the inner for-loop). This is known as the Shortest Path
+Faster Algorithm (SPFA). However, the worst case time complexity will
+degenerate to that of the Bellman-Ford algorithm - O(nm) on the number
+of nodes and edges respectively. In this case, the SPFA may still
+outperform the Bellman-Ford algorithm in the average case.
 
-Modification to Shortest Path Faster Algorithm: The code for Dijkstra's
-algorithm here can be easily modified to become the Shortest Path Faster
-Algorithm (SPFA) by simply commenting out "visit[a] = true;" and changing
-the priority queue to a FIFO queue like in BFS. SPFA is a faster version
-of the Bellman-Ford algorithm, working on negative path lengths (whereas
-Dijkstra's cannot). Certain graphs can be constructed to make SPFA slow.
+Time Complexity: dijkstra() is O(m log n) where m is the number of
+edges and n is the number of nodes. print_path() is O(n) on the number
+of nodes in the shortest path to be printed.
 
-=~=~=~=~= Sample Input =~=~=~=~=
-4 5
-0 1 2
-0 3 8
-1 2 2
-1 3 4
-2 3 1
-0 3
-
-=~=~=~=~= Sample Output =~=~=~=~=
-The shortest distance from 0 to 3 is 5.
-Take the path: 0->1->2->3.
+Space Complexity: O(n) auxiliary on the number of nodes.
 
 */
 
@@ -53,9 +38,9 @@ const int MAXN = 100, INF = 0x3f3f3f3f;
 int dist[MAXN], pred[MAXN];
 vector<pair<int, int> > adj[MAXN];
 
-void dijkstra(int nodes, int start) {
-  vector<bool> vis(nodes, false);
-  for (int i = 0; i < nodes; i++) {
+void dijkstra(int start) {
+  vector<bool> vis(MAXN, false);
+  for (int i = 0; i < MAXN; i++) {
     dist[i] = INF;
     pred[i] = -1;
   }
@@ -68,7 +53,8 @@ void dijkstra(int nodes, int start) {
     pq.pop();
     vis[u] = true;
     for (int j = 0; j < (int)adj[u].size(); j++) {
-      if (vis[v = adj[u][j].first]) continue;
+      if (vis[v = adj[u][j].first])
+        continue;
       if (dist[v] > dist[u] + adj[u][j].second) {
         dist[v] = dist[u] + adj[u][j].second;
         pred[v] = u;
@@ -78,24 +64,32 @@ void dijkstra(int nodes, int start) {
   }
 }
 
-//Use the precomputed pred[] array to print the path
 void print_path(int dest) {
   int i = 0, j = dest, path[MAXN];
-  while (pred[j] != -1) j = path[++i] = pred[j];
+  while (pred[j] != -1)
+    j = path[++i] = pred[j];
   cout << "Take the path: ";
-  while (i > 0) cout << path[i--] << "->";
+  while (i > 0)
+    cout << path[i--] << "->";
   cout << dest << ".\n";
 }
 
+/*** Example Usage
+
+Sample Output:
+The shortest distance from 0 to 3 is 5.
+Take the path: 0->1->2->3.
+
+***/
+
 int main() {
-  int nodes, edges, u, v, w, start, dest;
-  cin >> nodes >> edges;
-  for (int i = 0; i < edges; i++) {
-    cin >> u >> v >> w;
-    adj[u].push_back(make_pair(v, w));
-  }
-  cin >> start >> dest;
-  dijkstra(nodes, start);
+  int start = 0, dest = 3;
+  adj[0].push_back(make_pair(1, 2));
+  adj[0].push_back(make_pair(3, 8));
+  adj[1].push_back(make_pair(2, 2));
+  adj[1].push_back(make_pair(3, 4));
+  adj[2].push_back(make_pair(3, 1));
+  dijkstra(start);
   cout << "The shortest distance from " << start;
   cout << " to " << dest << " is " << dist[dest] << ".\n";
   print_path(dest);
