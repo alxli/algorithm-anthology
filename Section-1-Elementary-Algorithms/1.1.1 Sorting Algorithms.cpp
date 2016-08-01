c629c4a5589d2c2a21bb30c9f30bd44667422456
Alex Li <Alextrovert@gmail.com>
2016-08-01 02:57:22 -0400

    Updates

--- Section-1-Elementary-Algorithms/1.1.1 Sorting Algorithms.cpp
+++ Section-1-Elementary-Algorithms/1.1.1 Sorting Algorithms.cpp
@@ -1,12 +1,15 @@
 /*
 
-The following functions are to be used like std::sort(), taking two
-RandomAccessIterators as the range to be sorted, and optionally a
-comparison function object to replace the default < operator.
+These functions are equivalent to std::sort(), taking RandomAccessIterators
+as a range [lo, hi) to be sorted. Elements between lo and hi (including the
+element pointed to by lo but excluding the element pointed to by hi) will be
+sorted into ascending order after the function call. Optionally, a comparison
+function object specifying a strict weak ordering may be specified to replace
+the default operator<.
 
-They are not intended to compete with the standard library sorting
-functions in terms of speed, but are merely demonstrations of how to
-implement common sorting algorithms concisely in C++.
+These functions are not meant to compete with standard library implementations
+in terms terms of speed. Instead, they are meant to demonstrate how common
+sorting algorithms can be concisely implemented in C++.
 
 */
 
@@ -14,40 +17,40 @@ implement common sorting algorithms concisely in C++.
 #include <functional> /* std::less */
 #include <iterator>   /* std::iterator_traits */
 
+
 /*
 
 Quicksort
 
-Quicksort repeatedly selects a pivot and "partitions" the range so that
-all values comparing less than the pivot come before it, and all values
-comparing greater comes after it. Divide and conquer is then applied to
-both sides of the pivot until the original range is sorted. Despite
-having a worst case of O(n^2), quicksort is faster in practice than
-merge sort and heapsort, which each has a worst case of O(n log n).
+Quicksort repeatedly selects a pivot and partitions the range so that all
+elements comparing less than the pivot precedes the pivot, and all elements
+comparing greater follows it. Divide and conquer is then applied to both sides
+of the pivot until the original range is sorted. Despite having a worst case of
+O(n^2), quicksort is faster in practice than merge sort and heapsort, which both
+have a worst case time complexity of O(n log n).
 
-The pivot chosen in this implementation is always the middle element
-of the range to be sorted. To reduce the likelihood of encountering the
-worst case, the algorithm should be modified to select a random pivot,
-or use the "median of three" method.
+The pivot chosen in this implementation is always a middle element of the range
+to be sorted. To reduce the likelihood of encountering the worst case, the pivot
+can be chosen in better ways (e.g. randomly, or using the "median of three"
+technique).
 
 Time Complexity (Average): O(n log n)
 Time Complexity (Worst): O(n^2)
-Space Complexity: O(log n) auxiliary.
+Space Complexity: O(log n) auxiliary
 Stable?: No
 
 */
 
 template<class It, class Compare>
 void quicksort(It lo, It hi, Compare comp) {
-  if (hi - lo < 2) return;
+  if (hi - lo < 2)
+    return;
   typedef typename std::iterator_traits<It>::value_type T;
-  T pivot = *(lo + (hi - lo) / 2);
+  T pivot = *(lo + (hi - lo)/2);
   It i, j;
-  for (i = lo, j = hi - 1; ; ++i, --j) {
-    while (comp(*i, pivot))
-      ++i;
-    while (comp(pivot, *j))
-      --j;
+  for (i = lo, j = hi - 1; ; i++, j--) {
+    while (comp(*i, pivot)) i++;
+    while (comp(pivot, *j)) j--;
     if (i >= j)
       break;
     std::swap(*i, *j);
@@ -61,32 +64,33 @@ template<class It> void quicksort(It lo, It hi) {
   quicksort(lo, hi, std::less<T>());
 }
 
+
 /*
 
 Merge Sort
 
-Merge sort works by first dividing a list into n sublists, each with
-one element, then recursively merging sublists to produce new sorted
-sublists until only a single sorted sublist remains. Merge sort has a
-better worse case than quicksort, and is also stable, meaning that it
-will preserve the relative ordering of elements considered equal by
-the < operator or comparator (a < b and b < a both return false).
+Merge sort first divides a list into n sublists of one element each, then
+recursively merges the sublists into sorted order until only a single sorted
+sublist remains. Merge sort is a stable sort, meaning that it preserves the
+relative order of elements which compare equal by operator< or the custom
+comparator given.
 
-While std::stable_sort() is a corresponding function in the standard
-library, the implementation below differs in that it will simply fail
-if extra memory is not available. Meanwhile, std::stable_sort() will
-not fail, but instead fall back to a time complexity of O(n log^2 n).
+An analogous function in the C++ standard library is std::stable_sort(), except
+that the implementation here requires sufficient memory to be available. When
+O(n) auxiliary memory is not available, std::stable_sort() falls back to a time
+complexity of O(n log^2 n) but the implementation here will simply fail.
 
 Time Complexity (Average): O(n log n)
 Time Complexity (Worst): O(n log n)
-Space Complexity: O(n) auxiliary.
+Space Complexity: O(n) auxiliary
 Stable?: Yes
 
 */
 
 template<class It, class Compare>
 void mergesort(It lo, It hi, Compare comp) {
-  if (hi - lo < 2) return;
+  if (hi - lo < 2)
+    return;
   It mid = lo + (hi - lo - 1) / 2, a = lo, c = mid + 1;
   mergesort(lo, mid + 1, comp);
   mergesort(mid + 1, hi, comp);
@@ -111,55 +115,52 @@ template<class It> void mergesort(It lo, It hi) {
   mergesort(lo, hi, std::less<T>());
 }
 
+
 /*
 
 Heapsort
 
-Heapsort first rearranges an array to satisfy the heap property, and
-then the max element of the heap is repeated removed and added to the
-end of the resulting sorted list. A heapified array has the root node
-at index 0. The two children of the node at index n are respectively
-located at indices 2n + 1 and 2n + 2. Each node is greater than both
-of its children. This leads to a structure that takes O(log n) to
-insert any element or remove the max element. Heapsort has a better
-worst case complexity than quicksort, but a better space complexity
-complexity than merge sort.
+Heapsort first rearranges an array to satisfy the max-heap property. Then, it
+repeatedly pops the max element of the heap (the left, unsorted subrange),
+moving it to the beginning of the right, sorted subrange until the entire range
+is sorted. Heapsort has a better worst case time complexity than quicksort, but
+a better space complexity than merge sort.
 
-The standard library equivalent is calling std::make_heap(), followed
-by std::sort_heap() on the input range.
+The c++ standard library equivalent is calling std::make_heap(lo, hi), followed
+by std::sort_heap(lo, hi).
 
 Time Complexity (Average): O(n log n)
 Time Complexity (Worst): O(n log n)
-Space Complexity: O(1) auxiliary.
+Space Complexity: O(1) auxiliary
 Stable?: No
 
 */
 
 template<class It, class Compare>
 void heapsort(It lo, It hi, Compare comp) {
-  typename std::iterator_traits<It>::value_type t;
-  It i = lo + (hi - lo) / 2, j = hi, parent, child;
+  typename std::iterator_traits<It>::value_type tmp;
+  It i = lo + (hi - lo)/2, j = hi, parent, child;
   for (;;) {
     if (i <= lo) {
       if (--j == lo)
         return;
-      t = *j;
+      tmp = *j;
       *j = *lo;
     } else {
-      t = *(--i);
+      tmp = *(--i);
     }
     parent = i;
-    child = lo + 2 * (i - lo) + 1;
+    child = lo + 2*(i - lo) + 1;
     while (child < j) {
       if (child + 1 < j && comp(*child, *(child + 1)))
         child++;
-      if (!comp(t, *child))
+      if (!comp(tmp, *child))
         break;
       *parent = *child;
       parent = child;
-      child = lo + 2 * (parent - lo) + 1;
+      child = lo + 2*(parent - lo) + 1;
     }
-    *(lo + (parent - lo)) = t;
+    *(lo + (parent - lo)) = tmp;
   }
 }
 
@@ -168,24 +169,26 @@ template<class It> void heapsort(It lo, It hi) {
   heapsort(lo, hi, std::less<T>());
 }
 
+
 /*
 
 Comb Sort
 
-Comb sort is an improved bubble sort. While bubble sort increases the
-gap between swapped elements for every inner loop iteration, comb sort
-uses a fixed gap for the inner loop and decreases the gap size by a
-shrink factor for every iteration of the outer loop.
+Comb sort is an improved bubble sort. While bubble sort increments the gap
+between swapped elements for every inner loop iteration, comb sort fixes the gap
+size in the inner loop, decreasing it by a particular shrink factor in every
+iteration of the outer loop. The shrink factor of 1.3 is empirically determined
+to be the most effective.
 
-Even though the average time complexity is theoretically O(n^2), if the
-increments (gap sizes) are relatively prime and the shrink factor is
-sensible (1.3 is empirically determined to be the best), then it will
-require astronomically large n to make the algorithm exceed O(n log n)
-steps. In practice, comb sort is only 2-3 times slower than merge sort.
+Even though the worst case time complexity is theoretically O(n^2), a good
+shrink factor will ensure that the increments (gap sizes) are co-prime, in turn
+requiring astronomically large n to make the algorithm exceed O(n log n) steps.
+On random arrays, comb sort is only 2-3 times slower than merge sort. Thus, the
+small code length to efficiency ratio makes for a worthwhile algorithm to
+remember.
 
-Time Complexity (Average): O(n^2 / 2^p) for p increments.
 Time Complexity (Worst): O(n^2)
-Space Complexity: O(1) auxiliary.
+Space Complexity: O(1) auxiliary
 Stable?: No
 
 */
@@ -195,8 +198,9 @@ void combsort(It lo, It hi, Compare comp) {
   int gap = hi - lo;
   bool swapped = true;
   while (gap > 1 || swapped) {
-    if (gap > 1)
-      gap = (int)((float)gap / 1.3f);
+    if (gap > 1) {
+      gap = (int)((double)gap / 1.3);
+    }
     swapped = false;
     for (It i = lo; i + gap < hi; i++)
       if (comp(*(i + gap), *i)) {
@@ -211,30 +215,28 @@ template<class It> void combsort(It lo, It hi) {
   combsort(lo, hi, std::less<T>());
 }
 
+
 /*
 
 Radix Sort
 
-Radix sort can be used to sort integer keys with a constant number of
-bits in linear time. The keys are grouped by the individual digits of
-a particular base which share the same significant position and value.
+Radix sort is used to sort integer elements with a constant number of bits in
+linear time. This implementation only works on ranges pointing to unsigned
+integer primitives. The elements in the input range do not strictly have to be
+unsigned types, as long as their values are nonnegative integers.
 
-The implementation below only works on ranges pointing to unsigned
-integer primitives (but can be modified to also work on signed values).
-Note that the input range need not strictly be "unsigned" types, as
-long as the values are all technically non-negative. A power of two is
-chosen to be the base of the sort since bitwise operations may be used
-to extract digits (instead of modulos and powers, which are much less
-efficient). In practice, it's been demonstrated that 2^8 is the best
-choice for sorting 32-bit integers (roughly 5 times faster than using
-std::sort and 2 to 4 times faster than any other chosen power of two).
+In this implementation, a power of two is chosen to be the base for the sort
+so that bitwise operations can be easily used to extract digits. This avoids the
+neet to use modulo and exponentiation, which are much more expensive operations.
+In practice, it's been demonstrated that 2^8 is the best choice for sorting
+32-bit integers (approximately 5 times faster than std::sort(), and typically
+2-4 faster than radix sort using any other power of two chosen as the base).
 
 This implementation was adapted from: http://qr.ae/RbdDTa
 Explanation of base 2^8 choice: http://qr.ae/RbdDcG
 
-Time Complexity: O(n * w) for n integers of w bits.
-Space Complexity: O(n + w) auxiliary.
-Stable?: Yes
+Time Complexity: O(n*w) for n integers of w bits each
+Space Complexity: O(n + w) auxiliary
 
 */
 
@@ -243,9 +245,9 @@ void radix_sort(UnsignedIt lo, UnsignedIt hi) {
   if (hi - lo < 2)
     return;
   const int radix_bits = 8;
-  const int radix_base = 1 << radix_bits; //e.g. 2^8 = 256
-  const int radix_mask = radix_base - 1;  //e.g. 2^8 - 1 = 0xFF
-  int num_bits = 8 * sizeof(*lo); //8 bits per byte
+  const int radix_base = 1 << radix_bits;  // e.g. 2^8 = 256
+  const int radix_mask = radix_base - 1;  // e.g. 2^8 - 1 = 0xFF
+  int num_bits = 8 * sizeof(*lo);  // 8 bits per byte
   typedef typename std::iterator_traits<UnsignedIt>::value_type T;
   T *l = new T[hi - lo];
   for (int pos = 0; pos < num_bits; pos += radix_bits) {
@@ -262,18 +264,19 @@ void radix_sort(UnsignedIt lo, UnsignedIt hi) {
   delete[] l;
 }
 
+
 /*** Example Usage and Output:
 
 mergesort() with default comparisons: 1.32 1.41 1.62 1.73 2.58 2.72 3.14 4.67
 mergesort() with 'compare_as_ints()': 1.41 1.73 1.32 1.62 2.72 2.58 3.14 4.67
 ------
 Sorting five million integers...
-std::sort():  0.429s
-quicksort():  0.498s
-mergesort():  1.437s
-heapsort():   1.179s
-combsort():   1.023s
-radix_sort(): 0.078s
+std::sort():  0.355s
+quicksort():  0.426s
+mergesort():  1.263s
+heapsort():   1.093s
+combsort():   0.827s
+radix_sort(): 0.076s
 
 ***/
 
@@ -286,15 +289,17 @@ radix_sort(): 0.078s
 using namespace std;
 
 template<class It> void print_range(It lo, It hi) {
-  while (lo != hi)
+  while (lo != hi) {
     cout << *(lo++) << " ";
+  }
   cout << endl;
 }
 
 template<class It> bool is_sorted(It lo, It hi) {
-  while (++lo != hi)
+  while (++lo != hi) {
     if (*(lo - 1) > *lo)
       return false;
+  }
   return true;
 }
 
@@ -303,37 +308,37 @@ bool compare_as_ints(double i, double j) {
 }
 
 int main () {
-  { //can be used to sort arrays like std::sort()
+  {  // Can be used to sort arrays like std::sort().
     int a[] = {32, 71, 12, 45, 26, 80, 53, 33};
     quicksort(a, a + 8);
     assert(is_sorted(a, a + 8));
   }
-  { //STL containers work too
+  {  // STL containers work too.
     int a[] = {32, 71, 12, 45, 26, 80, 53, 33};
     vector<int> v(a, a + 8);
     quicksort(v.begin(), v.end());
     assert(is_sorted(v.begin(), v.end()));
   }
-  { //reverse iterators work as expected
+  {  // Reverse iterators work as expected.
     int a[] = {32, 71, 12, 45, 26, 80, 53, 33};
     vector<int> v(a, a + 8);
     heapsort(v.rbegin(), v.rend());
     assert(is_sorted(v.rbegin(), v.rend()));
   }
-  { //doubles are also fine
+  {  // We can sort doubles just as well.
     double a[] = {1.1, -5.0, 6.23, 4.123, 155.2};
     vector<double> v(a, a + 5);
     combsort(v.begin(), v.end());
     assert(is_sorted(v.begin(), v.end()));
   }
-  { //only unsigned ints work for radix_sort (but reverse works!)
+  {  // Must use radix_sort with unsigned values, but sorting in reverse works!
     int a[] = {32, 71, 12, 45, 26, 80, 53, 33};
     vector<int> v(a, a + 8);
     radix_sort(v.rbegin(), v.rend());
     assert(is_sorted(v.rbegin(), v.rend()));
   }
 
-  //example from http://www.cplusplus.com/reference/algorithm/stable_sort
+  // Example from: http://www.cplusplus.com/reference/algorithm/stable_sort
   double a[] = {3.14, 1.41, 2.72, 4.67, 1.73, 1.32, 1.62, 2.58};
   {
     vector<double> v(a, a + 8);
