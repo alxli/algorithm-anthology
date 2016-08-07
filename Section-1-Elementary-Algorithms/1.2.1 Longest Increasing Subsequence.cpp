ef5c5db81887442dcb7fa8ee767ce954d8115363
Alex Li <me@alexli.ca>
2016-08-06 21:42:40 -0400

    Update 1.2.1 Longest Increasing Subsequence.cpp

--- Section-1-Elementary-Algorithms/1.2.1 Longest Increasing Subsequence.cpp
+++ Section-1-Elementary-Algorithms/1.2.1 Longest Increasing Subsequence.cpp
@@ -1,52 +1,44 @@
 /*
 
-Given an array a[] of size n, determine a longest subsequence of a[]
-such that all of its elements are in ascending order. This subsequence
-is not necessarily contiguous or unique, so only one such answer will
-be found. The problem is efficiently solved using dynamic programming
-and binary searching, since it has the following optimal substructure
-with respect to the i-th position in the array:
-
+Given two RandomAccessIterators lo and hi specifying a range [lo, hi), determine
+a longest subsequence of the range such that all of its elements are in strictly
+ascending order. This subsequence is not necessarily contiguous or unique, so
+only one such answer will be found. The answer is efficiently found using binary
+search and dynamic programming. Let a[] be an array of the elements in [lo, hi)
+and suppose LIS[i] stores the length of the longest increasing subsequence for
+the first i values in a[]. Then, LIS[i] has the following optimal substructure:
   LIS[i] = 1 + max(LIS[j] for all j < i and a[j] < a[i])
   Otherwise if such a j does not exist, then LIS[i] = 1.
 
-Explanation: https://en.wikipedia.org/wiki/Longest_increasing_subsequence
-
-Time Complexity: O(n log n) on the size of the array.
+Time Complexity: O(n log n) on the distance between lo and hi.
 Space Complexity: O(n) auxiliary.
 
 */
 
+#include <iterator>  // std::iterator_traits
 #include <vector>
 
-std::vector<int> tail, prev;
-
-template<class T> int lower_bound(int len, T a[], int key) {
-  int lo = -1, hi = len;
-  while (hi - lo > 1) {
-    int mid = (lo + hi) / 2;
-    if (a[tail[mid]] < key)
-      lo = mid;
-    else
-      hi = mid;
-  }
-  return hi;
-}
-
-template<class T> std::vector<T> lis(int n, T a[]) {
-  tail.resize(n);
-  prev.resize(n);
-  int len = 0;
+template<class It> std::vector<typename std::iterator_traits<It>::value_type>
+longest_increasing_subsequence(It lo, It hi) {
+  int len = 0, n = hi - lo;
+  std::vector<int> prev(n), tail(n);
   for (int i = 0; i < n; i++) {
-    int pos = lower_bound(len, a, a[i]);
-    if (len < pos + 1)
-      len = pos + 1;
-    prev[i] = pos > 0 ? tail[pos - 1] : -1;
-    tail[pos] = i;
+    int l = -1, h = len;
+    while (h - l > 1) {
+      int mid = (l + h)/2;
+      if (*(lo + tail[mid]) < *(lo + i))
+        l = mid;
+      else
+        h = mid;
+    }
+    if (len < h + 1)
+      len = h + 1;
+    prev[i] = h > 0 ? tail[h - 1] : -1;
+    tail[h] = i;
   }
-  std::vector<T> res(len);
+  std::vector<typename std::iterator_traits<It>::value_type> res(len);
   for (int i = tail[len - 1]; i != -1; i = prev[i])
-    res[--len] = a[i];
+    res[--len] = *(lo + i);
   return res;
 }
 
@@ -67,7 +59,7 @@ template<class It> void print_range(It lo, It hi) {
 
 int main () {
   int a[] = {-2, -5, 1, 9, 10, 8, 11, 10, 13, 11};
-  vector<int> res = lis(10, a);
+  vector<int> res = longest_increasing_subsequence(a, a + 10);
   print_range(res.begin(), res.end());
   return 0;
 }
