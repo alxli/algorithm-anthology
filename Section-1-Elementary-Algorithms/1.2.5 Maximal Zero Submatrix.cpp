cac825f515de75c180ff133360baf147f8a9448b
Alex Li <Alextrovert@gmail.com>
2016-08-08 03:02:51 -0400

    Updates

--- Section-1-Elementary-Algorithms/1.2.5 Maximal Zero Submatrix.cpp
+++ Section-1-Elementary-Algorithms/1.2.5 Maximal Zero Submatrix.cpp
@@ -1,11 +1,9 @@
 /*
 
-Given an n by m rectangular matrix of 0's and 1's, determine the area
-of the largest rectangular submatrix which contains only 0's. This can
-be reduced the problem of finding the maximum rectangular area under a
-histogram, which can be efficiently solved using a stack. The following
-implementation accepts a 2-dimensional vector of bools and returns the
-area of the maximum zero submatrix.
+Given an n by m rectangular matrix of 0's and 1's as a two dimensional vector of
+bool, returns the area of the largest rectangular submatrix consisting of only
+0's. This can be reduced to the problem of finding the maximum rectangular area
+under a histogram, which is efficiently solved using a stack.
 
 Explanation: http://stackoverflow.com/a/13657337
 
@@ -14,33 +12,35 @@ Space Complexity: O(m) auxiliary.
 
 */
 
-#include <algorithm> /* std::max() */
+#include <algorithm>  // std::max()
+#include <stack>
 #include <vector>
 
-int max_zero_submatrix(const std::vector< std::vector<bool> > & mat) {
-  int n = mat.size(), m = mat[0].size(), res = 0;
-  std::vector<int> d(m, -1), d1(m), d2(m), stack;
+int max_zero_submatrix(const std::vector< std::vector<bool> > &matrix) {
+  int n = matrix.size(), m = matrix[0].size(), res = 0;
+  std::vector<int> d(m, -1), d1(m), d2(m);
   for (int r = 0; r < n; r++) {
     for (int c = 0; c < m; c++) {
-      if (mat[r][c])
+      if (matrix[r][c])
         d[c] = r;
     }
-    stack.clear();
+    std::stack<int> s;
     for (int c = 0; c < m; c++) {
-      while (!stack.empty() && d[stack.back()] <= d[c])
-        stack.pop_back();
-      d1[c] = stack.empty() ? -1 : stack.back();
-      stack.push_back(c);
+      while (!s.empty() && d[s.top()] <= d[c])
+        s.pop();
+      d1[c] = s.empty() ? -1 : s.top();
+      s.push(c);
     }
-    stack.clear();
+    while (!s.empty())
+      s.pop();
     for (int c = m - 1; c >= 0; c--) {
-      while (!stack.empty() && d[stack.back()] <= d[c])
-        stack.pop_back();
-      d2[c] = stack.empty() ? m : stack.back();
-      stack.push_back(c);
+      while (!s.empty() && d[s.top()] <= d[c])
+        s.pop();
+      d2[c] = s.empty() ? m : s.top();
+      s.push(c);
     }
     for (int j = 0; j < m; j++)
-      res = std::max(res, (r - d[j]) * (d2[j] - d1[j] - 1));
+      res = std::max(res, (r - d[j])*(d2[j] - d1[j] - 1));
   }
   return res;
 }
