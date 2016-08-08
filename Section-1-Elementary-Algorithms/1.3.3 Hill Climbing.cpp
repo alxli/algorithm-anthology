bd8b1eef5f3a84410304c46464888b8a3db11584
Alex Li <me@alexli.ca>
2016-08-08 15:41:45 -0400

    Update 1.3.3 Hill Climbing.cpp

--- Section-1-Elementary-Algorithms/1.3.3 Hill Climbing.cpp
+++ Section-1-Elementary-Algorithms/1.3.3 Hill Climbing.cpp
@@ -1,47 +1,44 @@
 /*
 
-Given a continuous function f on two real numbers, hill climbing is a
-technique that can be used to find the local maximum or minimum point
-based on some (possibly random) initial guess. Then, the algorithm
-considers taking a single step in each of a fixed number of directions.
-The direction with the best result is selected and steps are further
-taken there until the answer no longer improves. When this happens, the
-step size is reduced and the process repeats until a desired absolute
-error is reached. The result is not necessarily the global extrema, and
-the algorithm's success will heavily depend on the initial guess.
+Given a continuous function f(x, y) to double and a (possibly random) initial
+guess (x0, y0), returns a potential global minimum found through hill-climbing.
+Optionally, two double pointers x_ans and y_ans may be passed to store the input
+coordinates to f at which the returned minimum value is attained.
 
-The following function find_min() takes the function f, any starting
-guess (x0, y0), and optionally two pointers to double used for storing
-the answer coordinates. find_min() returns a local minimum point near
-the initial guess, and if the two pointers are given, then coordinates
-will be stored into the variables pointed to by x_ans and y_ans.
+Hill-climbing is a heuristic which starts at the guess, then considers taking
+a single step in each of a fixed number of directions. The direction with the
+best (in this case, minimum) value is chosen, and further steps are taken in it
+until the answer no longer improves. When this happens, the step size is reduced
+and the same process repeats until a desired absolute error is reached. The
+technique's success heavily depends on the behavior of f and the initial guess.
+Therefore, the result is not guaranteed to be the global minimum.
 
-Time Complexity: At most O(d log n) calls to f, where d is the number
-of directions considered at each position and n is the search space,
-roughly proportional to the largest possible step size divided by the
-smallest possible step size.
+Time Complexity: At most O(d log n) call will be made to f, where d is the
+number of directions considered at each position and n is the search space that
+is approximately proportional to the maximum possible step size divided by the
+minimum possible step size.
+
+Space Complexity: O(1) auxiliary.
 
 */
 
 #include <cmath>
-#include <iostream>
-using namespace std;
 
 template<class BinaryFunction>
 double find_min(BinaryFunction f, double x0, double y0,
                 double *x_ans = 0, double *y_ans = 0) {
-  static const double PI = acos(-1.0);
-  static const double STEP_MAX = 1000000;
-  static const double STEP_MIN = 1e-9;
-  static const int DIRECTIONS = 6;
+  const double PI = acos(-1.0);
+  const double STEP_MIN = 1e-9;
+  const double STEP_MAX = 1000000;
+  const int NUM_DIRECTIONS = 6;
   double x = x0, y = y0, res = f(x0, y0);
   for (double step = STEP_MAX; step > STEP_MIN; ) {
     double best = res, best_x = x, best_y = y;
     bool found = false;
-    for (int i = 0; i < DIRECTIONS; i++) {
-      double a = 2.0 * PI * i / DIRECTIONS;
-      double x2 = x + step * cos(a);
-      double y2 = y + step * sin(a);
+    for (int i = 0; i < NUM_DIRECTIONS; i++) {
+      double a = 2.0 * PI * i / NUM_DIRECTIONS;
+      double x2 = x + step*cos(a);
+      double y2 = y + step*sin(a);
       double val = f(x2, y2);
       if (best > val) {
         best_x = x2;
@@ -74,7 +71,7 @@ bool eq(double a, double b) {
   return fabs(a - b) < 1e-8;
 }
 
-//minimized at f(2, 3) = 0
+// Paraboloid with global minimum at f(2, 3) = 0.
 double f(double x, double y) {
   return (x - 2)*(x - 2) + (y - 3)*(y - 3);
 }
