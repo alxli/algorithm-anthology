ed63c115a551d2520f1e2bdbf9c548d2f22e055e
Alex Li <Alextrovert@gmail.com>
2016-08-08 02:33:03 -0400

    Updates

--- Section-1-Elementary-Algorithms/1.2.3 Majority Element (Boyer-Moore).cpp
+++ Section-1-Elementary-Algorithms/1.2.3 Majority Element (Boyer-Moore).cpp
@@ -1,19 +1,9 @@
 /*
 
-Given a sequence of n elements, the majority vote problem asks to find
-an element that occurs more frequently than all others, or determine
-that no such element exists. Formally, a value must occur strictly
-greater than floor(n/2) times to be considered the majority element.
-Boyer-Moore majority vote algorithm scans through the sequence and
-keeps track of a running counter for the most likely candidate so far.
-Whenever a value is equal to the current candidate, the counter is
-incremented, otherwise the counter is decremented. When the counter is
-zero, the candidate is eliminated and a new candidate is considered.
-
-The following implementation takes two random access iterators as the
-sequence [lo, hi) of elements and returns an iterator pointing to one
-instance of the majority element if it exists, or the iterator hi if
-there is no majority.
+Given two InputIterators lo and hi specifying a range [lo, hi) of n elements,
+returns a reference to the first occurrence of the majority element, or the
+iterator hi if there is no majority element. The majority element is defined as
+an element which occurs strictly greater than floor(n/2) times in the range.
 
 Time Complexity: O(n) on the size of the array.
 Space Complexty: O(1) auxiliary.
@@ -38,8 +28,9 @@ template<class It> It majority(It lo, It hi) {
     if (*it == *candidate)
       cnt++;
   }
-  if (cnt <= (hi - lo) / 2)
+  if (cnt <= (hi - lo)/2) {
     return hi;
+  }
   return candidate;
 }
 
