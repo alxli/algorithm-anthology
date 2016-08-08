68591114d9558b234d5ddd8d4975bb60fa360da3
Alex Li <me@alexli.ca>
2016-08-08 19:41:37 -0400

    Update 1.3.5 Convex Hull Trick (Fully Dynamic).cpp

--- Section-1-Elementary-Algorithms/1.3.5 Convex Hull Trick (Fully Dynamic).cpp
+++ Section-1-Elementary-Algorithms/1.3.5 Convex Hull Trick (Fully Dynamic).cpp
@@ -1,29 +1,22 @@
 /*
 
-Given a set of pairs (m, b) describing lines of the form y = mx + b,
-process a set of x-coordinate queries each asking to find the minimum
-y-value of any of the given lines when evaluated at the specified x.
-The convex hull optimization technique first ignores all lines which
-never take on the maximum at any x value, then sorts the rest in order
-of descending slope. The intersection points of adjacent lines in this
-sorted list form the upper envelope of a convex hull, and line segments
-connecting these points always take on the minimum y-value. The result
-can be split up into x-intervals each mapped to the line which takes on
-the minimum in that interval. The intervals can be binary search to
-solve each query in O(log n) time on the number of lines.
+Given a set of pairs (m, b) specifying lines of the form y = mx + b, process a
+set of x-coordinate queries each asking to find the minimum y-value when any of
+the given lines are evaluated at the specified x. To instead have the queries
+optimize for maximum y-value, set the QUERY_MAX flag to true.
 
-Explanation: http://wcipeg.com/wiki/Convex_hull_trick
+The following implementation is a fully dynamic variant of the convex hull
+optimization technique, using a self-balancing binary search tree (std::set) to
+support the ability to call add_line() and get_best() in any desired order.
 
-The following implementation is a fully dynamic version, using a
-self-balancing binary search tree (std::set) to support calling line
-addition and query operations in any desired order. In addition, one
-may instead optimize for maximum y by setting QUERY_MAX to true.
+Explanation: http://wcipeg.com/wiki/Convex_hull_trick#Fully_dynamic_variant
 
-Time Complexity: O(n log n) for n calls to add_line(), where each call
-is O(log n) amortized on the number of lines added so far. Each call to
-get_best() runs in O(log n) on the number of lines added so far.
+Time Complexity: O(n log n) on the total number of calls made to add_line(), for
+any interlaced sequence of add_line() and get_min() calls. Each add_line() call
+is O(log n) amortized time on the number of lines added so far. Each get_best()
+call runs in O(log n) on the number of lines added so far.
 
-Space Complexity: O(n) auxiliary on the number of calls to add_line().
+Space Complexity: O(n) auxiliary on the number of calls made to add_line().
 
 */
 
@@ -40,28 +33,24 @@ struct line {
   line(long long m, long long b) {
     this->m = m;
     this->b = b;
-    val = 0;
-    xlo = -INF;
-    is_query = false;
+    this->val = 0;
+    this->xlo = -INF;
+    this->is_query = false;
   }
 
-  long long evaluate(long long x) const {
-    return m * x + b;
-  }
-
-  bool parallel(const line & l) const {
+  bool parallel(const line &l) const {
     return m == l.m;
   }
 
-  double intersect(const line & l) const {
+  double intersect(const line &l) const {
     if (parallel(l))
       return INF;
     return (double)(l.b - b)/(m - l.m);
   }
 
-  bool operator < (const line & l) const {
+  bool operator< (const line &l) const {
     if (l.is_query)
-      return QUERY_MAX ? xlo < l.val : l.val < xlo;
+      return QUERY_MAX ? (xlo < l.val) : (l.val < xlo);
     return m < l.m;
   }
 };
@@ -75,17 +64,17 @@ bool has_prev(hulliter it) {
 }
 
 bool has_next(hulliter it) {
-  return it != hull.end() && ++it != hull.end();
+  return (it != hull.end()) && (++it != hull.end());
 }
 
 bool irrelevant(hulliter it) {
   if (!has_prev(it) || !has_next(it))
     return false;
-  hulliter prev = it; --prev;
-  hulliter next = it; ++next;
-  return QUERY_MAX ?
-          prev->intersect(*next) <= prev->intersect(*it) :
-          next->intersect(*prev) <= next->intersect(*it);
+  hulliter prev = it, next = it;
+  --prev;
+  ++next;
+  return QUERY_MAX ? prev->intersect(*next) <= prev->intersect(*it)
+                   : next->intersect(*prev) <= next->intersect(*it);
 }
 
 hulliter update_left_border(hulliter it) {
@@ -93,10 +82,10 @@ hulliter update_left_border(hulliter it) {
     return it;
   hulliter it2 = it;
   double val = it->intersect(QUERY_MAX ? *--it2 : *++it2);
-  line buf(*it);
-  buf.xlo = val;
+  line l(*it);
+  l.xlo = val;
   hull.erase(it++);
-  return hull.insert(it, buf);
+  return hull.insert(it, l);
 }
 
 void add_line(long long m, long long b) {
@@ -131,7 +120,7 @@ long long get_best(long long x) {
   hulliter it = hull.lower_bound(q);
   if (QUERY_MAX)
     --it;
-  return it->evaluate(x);
+  return it->m * x + it->b;
 }
 
 /*** Example Usage ***/
@@ -144,8 +133,8 @@ int main() {
   add_line(1, 2);
   add_line(2, 1);
   assert(get_best(0) == 0);
-  assert(get_best(1) == 3);
   assert(get_best(2) == 4);
+  assert(get_best(1) == 3);
   assert(get_best(3) == 5);
   return 0;
 }
