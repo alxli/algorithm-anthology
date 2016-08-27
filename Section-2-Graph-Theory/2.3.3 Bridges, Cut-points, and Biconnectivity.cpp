a712e17f6dccf51d99f0059826ed194870ecb240
Alex Li <Alextrovert@gmail.com>
2016-08-26 22:19:13 -0400

    Updates

--- Section-2-Graph-Theory/2.3.3 Bridges, Cut-points, and Biconnectivity.cpp
+++ Section-2-Graph-Theory/2.3.3 Bridges, Cut-points, and Biconnectivity.cpp
@@ -1,45 +1,46 @@
 /*
 
-Given an undirected graph, compute the following operations using
-Tarjan's algorithm.
+Given an undirected graph, compute the following properties of the graph using
+Tarjan's algorithm. tarjan() applies to a global, pre-populated adjacency list
+adj[] which satisfies the precondition that for every node v in adj[u], node u
+also exists in adj[v]. Nodes in adj[] must be numbered with integers between 0
+(inclusive) and the total number of nodes (exclusive), as passed in the function
+arguments. get_block_forest() applies to the global vector of biconnected
+components bcc[] which must have already been precomputed by a call to tarjan().
 
-A bridge is an edge such that when deleted, increases the number of
-connected components in the graph. An edge is a bridge if and only if
-it is not contained in any cycle.
+A bridge is an edge such that when deleted, the number of connected components
+in the graph is increased. An edge is a bridge if and only if it is not part of
+any cycle.
 
-A cut-point (i.e. cut-vertex or articulation point) is any vertex whose
-removal increases the number of connected components in the graph.
+A cut-point (i.e. cut-vertex, or articulation point) is any vertex whose removal
+increases the number of connected components in the graph.
 
-A biconnected component of a graph is a maximally biconnected subgraph.
-A biconnected graph is a connected and "nonseparable" graph, meaning
-that if any vertex were to be removed, the graph will remain connected.
-Therefore, a biconnected graph has no articulation vertices.
+A biconnected component of a graph is a maximally biconnected subgraph. A
+biconnected graph is a connected and "nonseparable" graph, meaning that if any
+vertex were to be removed, the graph will remain connected. Therefore, a
+biconnected graph has no articulation vertices.
 
-Any connected graph decomposes into a tree of biconnected components
-called the "block tree" of the graph. An unconnected graph will thus
-decompose into a "block forest."
+Any connected graph decomposes into a tree of biconnected components called the
+"block tree" of the graph. An unconnected graph will thus decompose into a
+"block forest."
 
 See: http://en.wikipedia.org/wiki/Biconnected_component
 
-Time Complexity: O(n) on the number of edges.
-
-Space Complexity: O(n) on the number of edges to store the input graph
-as an adjacency list and O(n) auxiliary on the number of nodes.
+Time Complexity: O(max(n, m)) on the number of nodes and edges.
+Space Complexity: O(max(n, m)) auxiliary on the number of nodes and edges.
 
 */
 
-#include <algorithm> /* std::fill(), std::min() */
-#include <iostream>
+#include <algorithm>  // std::fill(), std::min()
 #include <vector>
-using namespace std;
 
 const int MAXN = 100;
 int timer, lowlink[MAXN], tin[MAXN], comp[MAXN];
-vector<bool> vis(MAXN);
-vector<int> adj[MAXN], bcc_forest[MAXN];
-vector<int> stack, cutpoints;
-vector<vector<int> > bcc;
-vector<pair<int, int> > bridges;
+std::vector<bool> vis(MAXN);
+std::vector<int> adj[MAXN], bcc_forest[MAXN];
+std::vector<int> stack, cutpoints;
+std::vector<std::vector<int> > bcc;
+std::vector<std::pair<int, int> > bridges;
 
 void dfs(int u, int p) {
   vis[u] = true;
@@ -48,24 +49,28 @@ void dfs(int u, int p) {
   int v, children = 0;
   bool cutpoint = false;
   for (int j = 0; j < (int)adj[u].size(); j++) {
-    if ((v = adj[u][j]) == p)
+    v = adj[u][j];
+    if (v == p)
       continue;
     if (vis[v]) {
-      //lowlink[u] = min(lowlink[u], lowlink[v]);
-      lowlink[u] = min(lowlink[u], tin[v]);
+      lowlink[u] = std::min(lowlink[u], tin[v]);
     } else {
       dfs(v, u);
-      lowlink[u] = min(lowlink[u], lowlink[v]);
+      lowlink[u] = std::min(lowlink[u], lowlink[v]);
       cutpoint |= (lowlink[v] >= tin[u]);
       if (lowlink[v] > tin[u])
-        bridges.push_back(make_pair(u, v));
+        bridges.push_back(std::make_pair(u, v));
       children++;
     }
   }
-  if (p == -1) cutpoint = (children >= 2);
-  if (cutpoint) cutpoints.push_back(u);
+  if (p == -1) {
+    cutpoint = (children >= 2);
+  }
+  if (cutpoint) {
+    cutpoints.push_back(u);
+  }
   if (lowlink[u] == tin[u]) {
-    vector<int> component;
+    std::vector<int> component;
     do {
       v = stack.back();
       stack.pop_back();
@@ -80,9 +85,9 @@ void tarjan(int nodes) {
   bridges.clear();
   cutpoints.clear();
   stack.clear();
-  fill(lowlink, lowlink + nodes, 0);
-  fill(tin, tin + nodes, 0);
-  fill(vis.begin(), vis.end(), false);
+  std::fill(lowlink, lowlink + nodes, 0);
+  std::fill(tin, tin + nodes, 0);
+  std::fill(vis.begin(), vis.end(), false);
   timer = 0;
   for (int i = 0; i < nodes; i++) {
     if (!vis[i])
@@ -90,17 +95,21 @@ void tarjan(int nodes) {
   }
 }
 
-void get_block_tree(int nodes) {
-  fill(comp, comp + nodes, 0);
-  for (int i = 0; i < nodes; i++)
+void get_block_forest(int nodes) {
+  std::fill(comp, comp + nodes, 0);
+  for (int i = 0; i < nodes; i++) {
     bcc_forest[i].clear();
-  for (int i = 0; i < (int)bcc.size(); i++)
+  }
+  for (int i = 0; i < (int)bcc.size(); i++) {
     for (int j = 0; j < (int)bcc[i].size(); j++)
       comp[bcc[i][j]] = i;
-  for (int i = 0; i < nodes; i++)
-    for (int j = 0; j < (int)adj[i].size(); j++)
+  }
+  for (int i = 0; i < nodes; i++) {
+    for (int j = 0; j < (int)adj[i].size(); j++) {
       if (comp[i] != comp[adj[i][j]])
         bcc_forest[comp[i]].push_back(comp[adj[i][j]]);
+    }
+  }
 }
 
 /*** Example Usage and Output:
@@ -127,15 +136,23 @@ Adjacency List for Block Forest:
 
 ***/
 
+#include <iostream>
+using namespace std;
+
+void add_edge(int u, int v) {
+  adj[u].push_back(v);
+  adj[v].push_back(u);
+}
+
 int main() {
-  adj[0].push_back(1); adj[0].push_back(1);
-  adj[0].push_back(5); adj[5].push_back(0);
-  adj[1].push_back(2); adj[2].push_back(1);
-  adj[1].push_back(5); adj[5].push_back(1);
-  adj[3].push_back(7); adj[7].push_back(3);
-  adj[4].push_back(5); adj[5].push_back(4);
+  add_edge(0, 1);
+  add_edge(0, 5);
+  add_edge(1, 2);
+  add_edge(1, 5);
+  add_edge(3, 7);
+  add_edge(4, 5);
   tarjan(8);
-  get_block_tree(8);
+  get_block_forest(8);
   cout << "Cut-points:";
   for (int i = 0; i < (int)cutpoints.size(); i++)
     cout << " " << cutpoints[i];
