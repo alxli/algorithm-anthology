67a5a59879f43ab1482d7c782afdacfaf9ecbf34
Alex Li <alextrovert@gmail.com>
2017-02-10 19:08:57 -0500

    Updates

--- Section-3-Data-Structures/3.3.2 Segment Tree.cpp
+++ Section-3-Data-Structures/3.3.2 Segment Tree.cpp
@@ -21,82 +21,114 @@ indices from 0 to N - 1, inclusive, are accessible.
 #include <limits> /* std::numeric_limits<T>::min() */
 #include <vector>
 
+#include <cstdio>
+
 template<class T> class segment_tree {
-  int len, x, y;
+
+  static T identity() {
+    return std::numeric_limits<T>::min();
+  }
+
+  static T join_values(const T &a, const T &b) {
+    return a > b ? a : b;
+  }
+
+  int len;
   std::vector<T> t;
-  T val, *init;
 
-  //define the following yourself. merge(x, nullv) must return x for all x
-  static inline T nullv() { return std::numeric_limits<T>::min(); }
-  static inline T merge(const T & a, const T & b) { return a > b ? a : b; }
+  // Temporary helper variables.
+  int x, y;
+  T val;
 
-  void build(int n, int lo, int hi) {
+  template<class It>
+  void build(int n, int lo, int hi, bool init_with_arr, It arr) {
     if (lo == hi) {
-      t[n] = init[lo];
+      t[n] = init_with_arr ? *(arr + lo) : val;
       return;
     }
-    build(n * 2 + 1, lo, (lo + hi) / 2);
-    build(n * 2 + 2, (lo + hi) / 2 + 1, hi);
-    t[n] = merge(t[n * 2 + 1], t[n * 2 + 2]);
+    build(n*2 + 1, lo, (lo + hi)/2, init_with_arr, arr);
+    build(n*2 + 2, (lo + hi)/2 + 1, hi, init_with_arr, arr);
+    t[n] = join_values(t[n*2 + 1], t[n*2 + 2]);
   }
 
   void update(int n, int lo, int hi) {
-    if (x < lo || x > hi) return;
+    if (x < lo || x > hi) {
+      return;
+    }
     if (lo == hi) {
       t[n] = val;
       return;
     }
-    update(n * 2 + 1, lo, (lo + hi) / 2);
-    update(n * 2 + 2, (lo + hi) / 2 + 1, hi);
-    t[n] = merge(t[n * 2 + 1], t[n * 2 + 2]);
+    update(n*2 + 1, lo, (lo + hi)/2);
+    update(n*2 + 2, (lo + hi)/2 + 1, hi);
+    t[n] = join_values(t[n*2 + 1], t[n*2 + 2]);
   }
 
   T query(int n, int lo, int hi) {
-    if (hi < x || lo > y) return nullv();
-    if (lo >= x && hi <= y) return t[n];
-    return merge(query(n * 2 + 1, lo, (lo + hi) / 2),
-                 query(n * 2 + 2, (lo + hi) / 2 + 1, hi));
+    if (hi < x || lo > y) {
+      return identity();
+    }
+    if (lo >= x && hi <= y) {
+      return t[n];
+    }
+    return join_values(query(n*2 + 1, lo, (lo + hi)/2),
+                       query(n*2 + 2, (lo + hi)/2 + 1, hi));
   }
 
  public:
-  segment_tree(int n, T * a = 0): len(n), t(4 * n, nullv()) {
-    if (a != 0) {
-      init = a;
-      build(0, 0, len - 1);
-    }
+  segment_tree(int n, const T &v = T()) {
+    len = n;
+    t.resize(4*len, identity());
+    val = v;
+    build<T*>(0, 0, len - 1, false, 0);
   }
 
-  //a[i] = v
-  void update(int i, const T & v) {
+  template<class It> segment_tree(It lo, It hi) {
+    len = hi - lo;
+    t.resize(4*len, identity());
+    build<It>(0, 0, len - 1, true, lo);
+  }
+
+  int size() const {
+    return len;
+  }
+
+  T at(int i) {
+    return query(i, i);
+  }
+
+  void update(int i, const T &v) {
     x = i;
     val = v;
     update(0, 0, len - 1);
   }
 
-  //merge(a[i] for i = lo..hi, inclusive)
   T query(int lo, int hi) {
     x = lo;
     y = hi;
     return query(0, 0, len - 1);
   }
-
-  inline int size() { return len; }
-  inline T at(int i) { return query(i, i); }
 };
 
-/*** Example Usage ***/
+/*** Example Usage and Output:
+
+Values: 6 4 1 8 10
+The max value in the range [0, 3] is 8.
+
+***/
 
 #include <iostream>
 using namespace std;
 
 int main() {
   int arr[5] = {6, -2, 1, 8, 10};
-  segment_tree<int> T(5, arr);
-  T.update(1, 4);
-  cout << "Array contains:";
-  for (int i = 0; i < T.size(); i++)
-    cout << " " << T.at(i);
-  cout << "\nThe max value in the range [0, 3] is ";
-  cout << T.query(0, 3) << ".\n"; //8
+  segment_tree<int> t(arr, arr + 5);
+  t.update(1, 4);
+  cout << "Values:";
+  for (int i = 0; i < t.size(); i++) {
+    cout << " " << t.at(i);
+  }
+  cout << endl << "The max value in the range [0, 3] is "
+       << t.query(0, 3) << "." << endl;
   return 0;
 }
