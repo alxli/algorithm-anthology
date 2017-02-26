c5ca71fb3ad5bea5e24e68f349ced3d214428233
Alex Li <Alextrovert@gmail.com>
2017-02-26 04:57:01 -0500

    Updates

--- Section-3-Data-Structures/3.3.6 Sparse Table (RMQ).cpp
+++ Section-3-Data-Structures/3.3.6 Sparse Table (RMQ).cpp
@@ -1,61 +1,59 @@
 /*
 
-Description: The static range minimum query problem can be solved
-using a sparse table data structure. The RMQ for sub arrays of
-length 2^k is pre-processed using dynamic programming with formula:
+Given a static array with indices from 0 to n - 1, precompute a table that may
+later be used perform minimum range queries on the array in constant time. This
+version is simplified to only work on integer arrays.
 
-dp[i][j] = dp[i][j-1], if A[dp[i][j-1]] <= A[dp[i+2^(j-1)-1][j-1]]
-           dp[i+2^(j-1)-1][j-1], otherwise
+The dynamic programming state dp[i][j] holds the index of the minimum value in
+the sub-array starting at i and having length 2^j. Each dp[i][j] will always be
+equal to either dp[i][j - 1] or dp[i + 2^(j - 1) - 1)][j - 1], whichever of the
+indices corresponds to the smaller value in the array.
 
-where dp[i][j] is the index of the minimum value in the sub array
-starting at i having length 2^j.
+Time Complexity:
+- O(n log n) per call to build_rmq_table(), where n is the size of the array.
+- O(1) per call to query_min().
 
-Time Complexity: O(N log N) for build() and O(1) for min_idx()
-Space Complexity: O(N log N) on the size of the array.
-
-Note: This implementation is 0-based, meaning that all
-indices from 0 to N - 1, inclusive, are valid.
+Space Complexity:
+- O(n log n) to store the sparse table, where n is the size of the array.
 
 */
 
 #include <vector>
 
-const int MAXN = 100;
-std::vector<int> logtable, dp[MAXN];
+const int MAXN = 1000;
+std::vector<int> table, dp[MAXN];
 
-void build(int n, int a[]) {
-  logtable.resize(n + 1);
+void build_rmq_table(int n, int a[]) {
+  table.resize(n + 1);
   for (int i = 2; i <= n; i++)
-    logtable[i] = logtable[i >> 1] + 1;
+    table[i] = table[i >> 1] + 1;
   for (int i = 0; i < n; i++) {
-    dp[i].resize(logtable[n] + 1);
+    dp[i].resize(table[n] + 1);
     dp[i][0] = i;
   }
-  for (int k = 1; (1 << k) < n; k++) {
-    for (int i = 0; i + (1 << k) <= n; i++) {
-      int x = dp[i][k - 1];
-      int y = dp[i + (1 << (k - 1))][k - 1];
-      dp[i][k] = a[x] <= a[y] ? x : y;
+  for (int j = 1; (1 << j) < n; j++) {
+    for (int i = 0; i + (1 << j) <= n; i++) {
+      int x = dp[i][j - 1];
+      int y = dp[i + (1 << (j - 1))][j - 1];
+      dp[i][j] = a[x] < a[y] ? x : y;
     }
   }
 }
 
-//returns index of min element in [lo, hi]
-int min_idx(int a[], int lo, int hi) {
-  int k = logtable[hi - lo];
-  int x = dp[lo][k];
-  int y = dp[hi - (1 << k) + 1][k];
-  return a[x] <= a[y] ? x : y;
+int query_min(int a[], int lo, int hi) {
+  int j = table[hi - lo];
+  int x = dp[lo][j];
+  int y = dp[hi - (1 << j) + 1][j];
+  return a[x] < a[y] ? a[x] : a[y];
 }
 
 /*** Example Usage ***/
 
-#include <iostream>
-using namespace std;
+#include <cassert>
 
 int main() {
-  int a[] = {7, -10, 5, 20};
-  build(4, a);
-  cout << min_idx(a, 0, 3) << "\n"; //1
+  int arr[5] = {6, -2, 1, 8, 10};
+  build_rmq_table(5, arr);
+  assert(-2 == query_min(arr, 0, 4));
   return 0;
 }
