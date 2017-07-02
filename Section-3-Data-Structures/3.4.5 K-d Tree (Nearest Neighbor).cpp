24d83a85c25b2ac3f485fd2455e36c7cba4278a9
Alex Li <me@alexli.ca>
2017-07-02 16:01:12 -0700

    Updates

--- Section-3-Data-Structures/3.4.5 K-d Tree (Nearest Neighbor).cpp
+++ Section-3-Data-Structures/3.4.5 K-d Tree (Nearest Neighbor).cpp
@@ -1,108 +1,131 @@
 /*
 
-Description: k-d tree (short for k-dimensional tree) is a space-
-partitioning data structure for organizing points in a k-
-dimensional space. The following implementation supports
-querying the nearest neighboring point to (x, y) in terms of
-Euclidean distance after the tree has been build. Note that
-a point is not considered its own neighbour if it already exists
-in the tree.
+Maintain a set of two-dimensional points while supporting queries for the
+closest point in the set to a given query point. This implementation uses
+std::pair to represent points, requiring operators <, ==, -, and long double
+casting to be defined on the numeric template type.
 
-Time Complexity: O(N log N) for build(), where N is the number of
-points in the tree. nearest_neighbor_id() is O(log(N)) on average.
+- kd_tree(lo, hi) constructs a set from two RandomAccessIterators to std::pair
+  as a range [lo, hi) of points.
+- nearest(x, y, can_equal) returns a point in the set that is closest to (x, y)
+  by Euclidean distance. This may be equal to (x, y) only if can_equal is true.
 
-Space Complexity: O(N) on the number of points.
+Time Complexity:
+- O(n log n) per call to the constructor, where n is the number of points.
+- O(log n) on average per call to nearest().
+
+Space Complexity:
+- O(n) for storage of the points.
+- O(log n) auxiliary stack space per call to nearest().
 
 */
 
 #include <algorithm>
-#include <climits>
+#include <limits>
+#include <stdexcept>
 #include <utility>
 #include <vector>
 
-class kd_tree {
-  typedef std::pair<int, int> point;
+template<class T> class kd_tree {
+  typedef std::pair<T, T> point;
 
-  static inline bool cmp_x(const point & a, const point & b) {
+  static inline bool comp1(const point &a, const point &b) {
     return a.first < b.first;
   }
 
-  static inline bool cmp_y(const point & a, const point & b) {
+  static inline bool comp2(const point &a, const point &b) {
     return a.second < b.second;
   }
 
-  std::vector<int> tx, ty;
+  std::vector<point> tree;
   std::vector<bool> div_x;
 
-  void build(int lo, int hi, point P[]) {
-    if (lo >= hi) return;
-    int mid = (lo + hi) >> 1;
-    int minx = INT_MAX, maxx = INT_MIN;
-    int miny = INT_MAX, maxy = INT_MIN;
-    for (int i = lo; i < hi; i++) {
-      minx = std::min(minx, P[i].first);
-      maxx = std::max(maxx, P[i].first);
-      miny = std::min(miny, P[i].second);
-      maxy = std::max(maxy, P[i].second);
+  void build(int lo, int hi) {
+    if (lo >= hi) {
+      return;
     }
-    div_x[mid] = (maxx - minx) >= (maxy - miny);
-    std::nth_element(P + lo, P + mid, P + hi, div_x[mid] ? cmp_x : cmp_y);
-    tx[mid] = P[mid].first;
-    ty[mid] = P[mid].second;
-    if (lo + 1 == hi) return;
-    build(lo, mid, P);
-    build(mid + 1, hi, P);
+    int mid = (lo + hi)/2;
+    T minx, maxx, miny, maxy;
+    minx = maxx = tree[lo].first;
+    miny = maxy = tree[lo].second;
+    for (int i = lo + 1; i < hi; i++) {
+      minx = std::min(minx, tree[i].first);
+      miny = std::min(miny, tree[i].second);
+      maxx = std::max(maxx, tree[i].first);
+      maxy = std::max(maxy, tree[i].second);
+    }
+    div_x[mid] = !((maxx - minx) < (maxy - miny));
+    std::nth_element(tree.begin() + lo, tree.begin() + mid, tree.begin() + hi,
+                     div_x[mid] ? comp1 : comp2);
+    if (lo + 1 == hi) {
+      return;
+    }
+    build(lo, mid);
+    build(mid + 1, hi);
   }
 
-  long long min_dist;
-  int min_dist_id, x, y;
+  // Helper variables for nearest().
+  long double min_dist;
+  int id;
 
-  void nearest_neighbor(int lo, int hi) {
-    if (lo >= hi) return;
-    int mid = (lo + hi) >> 1;
-    int dx = x - tx[mid], dy = y - ty[mid];
-    long long d = dx*(long long)dx + dy*(long long)dy;
-    if (min_dist > d && d) {
-      min_dist = d;
-      min_dist_id = mid;
+  void nearest(int lo, int hi, const T &x, const T &y, bool can_equal) {
+    if (lo >= hi) {
+      return;
     }
-    if (lo + 1 == hi) return;
-    int delta = div_x[mid] ? dx : dy;
-    long long delta2 = delta*(long long)delta;
+    int mid = (lo + hi)/2;
+    T dx = x - tree[mid].first, dy = y - tree[mid].second;
+    long double d = dx*(long double)dx + dy*(long double)dy;
+    if (d < min_dist && (can_equal || d != 0)) {
+      min_dist = d;
+      id = mid;
+    }
+    if (lo + 1 == hi) {
+      return;
+    }
+    d = (long double)(div_x[mid] ? dx : dy);
     int l1 = lo, r1 = mid, l2 = mid + 1, r2 = hi;
-    if (delta > 0) std::swap(l1, l2), std::swap(r1, r2);
-    nearest_neighbor(l1, r1);
-    if (delta2 < min_dist) nearest_neighbor(l2, r2);
+    if (d > 0) {
+      std::swap(l1, l2);
+      std::swap(r1, r2);
+    }
+    nearest(l1, r1, x, y, can_equal);
+    if (d*(long double)d < min_dist) {
+      nearest(l2, r2, x, y, can_equal);
+    }
   }
 
  public:
-  kd_tree(int N, point P[]) {
-    tx.resize(N);
-    ty.resize(N);
-    div_x.resize(N);
-    build(0, N, P);
+  template<class It> kd_tree(It lo, It hi) {
+    int n = std::distance(lo, hi);
+    if (n <= 1) {
+      throw std::runtime_error("K-d tree must be have at least 2 points.");
+    }
+    tree.assign(lo, hi);
+    div_x.resize(n);
+    build(0, n);
   }
 
-  int nearest_neighbor_id(int x, int y) {
-    this->x = x; this->y = y;
-    min_dist = LLONG_MAX;
-    nearest_neighbor(0, tx.size());
-    return min_dist_id;
+  point nearest(const T &x, const T &y, bool can_equal = true) {
+    min_dist = std::numeric_limits<long double>::max();
+    nearest(0, tree.size(), x, y, can_equal);
+    return tree[id];
   }
 };
 
 /*** Example Usage ***/
 
-#include <iostream>
+#include <cassert>
 using namespace std;
 
 int main() {
-  pair<int, int> P[3];
-  P[0] = make_pair(0, 2);
-  P[1] = make_pair(0, 3);
-  P[2] = make_pair(-1, 0);
-  kd_tree T(3, P);
-  int res = T.nearest_neighbor_id(0, 0);
-  cout << P[res].first << " " << P[res].second << "\n"; //-1, 0
+  pair<int, int> p[3];
+  p[0] = make_pair(0, 2);
+  p[1] = make_pair(0, 3);
+  p[2] = make_pair(-1, 0);
+  kd_tree<int> t(p, p + 3);
+  assert(t.nearest(0, 2, true) == make_pair(0, 2));
+  assert(t.nearest(0, 2, false) == make_pair(0, 3));
+  assert(t.nearest(0, 0) == make_pair(-1, 0));
+  assert(t.nearest(-10000, 0) == make_pair(-1, 0));
   return 0;
 }
