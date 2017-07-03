017708bbd5d98ef6ff8b44a0422559ee13336c5d
Alex Li <me@alexli.ca>
2017-07-02 21:46:13 -0700

    Updates

--- Section-3-Data-Structures/3.6.3 Lowest Common Ancestor (Sparse Table).cpp
+++ Section-3-Data-Structures/3.6.3 Lowest Common Ancestor (Sparse Table).cpp
@@ -1,17 +1,21 @@
 /*
 
-Description: Given an undirected graph that is a tree, the
-lowest common ancestor (LCA) of two nodes v and w is the
-lowest (i.e. deepest) node that has both v and w as descendants,
-where we define each node to be a descendant of itself (so if
-v has a direct connection from w, w is the lowest common
-ancestor). The following program uses sparse tables to solve
-the problem on an unchanging graph.
+Given a tree, determine the lowest common ancestor of any two nodes in the tree.
+The lowest common ancestor of two nodes u and v is the node that has the longest
+distance from the root while having both u and v as its descendant. A nodes is
+considered to be a descendant of itself. build() applies to a global,
+pre-populated adjacency list adj[] which must only consist of nodes numbered
+with integers between 0 (inclusive) and the total number of nodes (exclusive),
+as passed in the function argument.
 
-Time Complexity: O(N log N) for build() and O(log N) for lca(),
-where N is the number of nodes in the tree.
+Time Complexity:
+- O(n log n) per call to build(), where n is the number of nodes.
+- O(log n) per call to lca().
 
-Space Complexity: O(N log N).
+Space Complexity:
+- O(n log n) to store the sparse table, where n is the number of nodes.
+- O(n) auxiliary stack space for build().
+- O(1) auxiliary stack space for lca().
 
 */
 
@@ -24,39 +28,52 @@ std::vector<int> adj[MAXN], dp[MAXN];
 void dfs(int u, int p) {
   tin[u] = timer++;
   dp[u][0] = p;
-  for (int i = 1; i < len; i++)
+  for (int i = 1; i < len; i++) {
     dp[u][i] = dp[dp[u][i - 1]][i - 1];
-  for (int j = 0, v; j < (int)adj[u].size(); j++)
-    if ((v = adj[u][j]) != p)
+  }
+  for (int j = 0; j < (int)adj[u].size(); j++) {
+    int v = adj[u][j];
+    if (v != p) {
       dfs(v, u);
+    }
+  }
   tout[u] = timer++;
 }
 
-void build(int nodes, int root) {
+void build(int nodes, int root = 0) {
   len = 1;
-  while ((1 << len) <= nodes) len++;
-  for (int i = 0; i < nodes; i++)
+  while ((1 << len) <= nodes) {
+    len++;
+  }
+  for (int i = 0; i < nodes; i++) {
     dp[i].resize(len);
+  }
   timer = 0;
   dfs(root, root);
 }
 
-inline bool is_parent(int parent, int child) {
+bool is_child(int parent, int child) {
   return tin[parent] <= tin[child] && tout[child] <= tout[parent];
 }
 
-int lca(int a, int b) {
-  if (is_parent(a, b)) return a;
-  if (is_parent(b, a)) return b;
-  for (int i = len - 1; i >= 0; i--)
-    if (!is_parent(dp[a][i], b))
-      a = dp[a][i];
-  return dp[a][0];
+int lca(int u, int v) {
+  if (is_child(u, v)) {
+    return u;
+  }
+  if (is_child(v, u)) {
+    return v;
+  }
+  for (int i = len - 1; i >= 0; i--) {
+    if (!is_child(dp[u][i], v)) {
+      u = dp[u][i];
+    }
+  }
+  return dp[u][0];
 }
 
 /*** Example Usage ***/
 
-#include <iostream>
+#include <cassert>
 using namespace std;
 
 int main() {
@@ -69,7 +86,7 @@ int main() {
   adj[0].push_back(4);
   adj[4].push_back(0);
   build(5, 0);
-  cout << lca(3, 2) << "\n"; //1
-  cout << lca(2, 4) << "\n"; //0
+  assert(lca(3, 2) == 1);
+  assert(lca(2, 4) == 0);
   return 0;
 }
