f67eb8c7eb18c8cd1d021297dc0fbfd78629478e
Alex Li <me@alexli.ca>
2017-07-03 04:45:25 -0700

    Updates

--- Section-3-Data-Structures/3.6.6 Link-Cut Tree.cpp
+++ Section-3-Data-Structures/3.6.6 Link-Cut Tree.cpp
@@ -1,227 +1,255 @@
 /*
 
-Description: Given an unweighted forest of trees where each node
-has an associated value, a link/cut tree can be used to dynamically
-query and modify values on the path between pairs of nodes a tree.
-This problem can be solved using heavy-light decomposition, which
-also supports having values stored on edges rather than the nodes.
-However in a link/cut tree, nodes in different trees may be
-dynamically linked, edges between nodes in the same tree may be
-dynamically split, and connectivity between two nodes (whether they
-are in the same tree) may be checked.
+Maintain a forest of trees with values associated with either its nodes, while
+supporting both dynamic queries and dynamic updates of all values on any path
+between two nodes in a given tree. In addition, support testing of whether two
+nodes are connected in the forest, as well as the merging and spliting of trees
+by adding or removing specific edges.
 
-Time Complexity: O(log N) amortized for make_root(), link(), cut(),
-connected(), modify(), and query(), where N is the number of nodes
-in the forest.
+The query operation is defined by an associative join_values() function which
+satisfies join_values(x, join_values(y, z)) = join_values(join_values(x, y), z)
+for all values x, y, and z in the forest. The default code below assumes a
+numerical forest type, defining queries for the "min" of the target range.
+Another possible query operation is "sum", in which case the join_values()
+function should defined to return "a + b".
 
-Space Complexity: O(N) on the number of nodes in the forest.
+The update operation is defined by the join_value_with_delta() and join_deltas()
+functions, which determines the change made to values. These must satisfy:
+- join_deltas(d1, join_deltas(d2, d3)) = join_deltas(join_deltas(d1, d2), d3).
+- join_value_with_delta(join_values(v, ...(m times)..., v), d, m)) should be
+  equal to join_values(join_value_with_delta(v, d, 1), ...(m times)).
+- if a sequence d_1, ..., d_m of deltas is used to update a value v, then
+  join_value_with_delta(v, join_deltas(d_1, ..., d_m), 1) should be equivalent
+  to m sequential calls to join_value_with_delta(v, d_i, 1) for i = 1..m.
+The default code below defines updates that "set" a path's nodes to a new value.
+Another possible update operation is "increment", in which case
+join_value_with_delta(v, d, len) should be defined to return "v + d*len" and
+join_deltas(d1, d2) should be defined to return "d1 + d2".
+
+- link_cut_forest() constructs an empty forest with no trees.
+- size() returns the number of nodes in the forest.
+- trees() returns the number of trees in the forest.
+- make_root(i, v) creates a new tree in the forest consisting of a single node
+  labeled with the integer i and value initialized to v.
+- is_connected(a, b) returns whether nodes a and b are connected.
+- link(a, b) adds an edge between the nodes a and b, both of which must exist
+  and not be connected.
+- cut(a, b) removes the edge between the nodes a and b, both of which must
+  exist and be connected.
+- query(a, b) returns the result of join_values() applied to all values on the
+  path from the node a to node b.
+- update(a, b, d) modifies all the values on the path from node a to node b by
+  respectively joining them with d using join_value_with_delta().
+
+Time Complexity:
+- O(1) per call to the constructor, size(), and trees().
+- O(log n) amortized per call to all other operations, where n is the number of
+  nodes.
+
+Space Complexity:
+- O(n) for storage of the forest, where n is the number of nodes.
+- O(1) auxiliary for all operations.
 
 */
 
 #include <algorithm>
+#include <cstdlib>
 #include <climits>
 #include <map>
 #include <stdexcept>
 
-template<class T> class linkcut_forest {
-  //Modify the following 5 functions to implement your custom
-  //operations on the tree. This implements the Add/Max operations.
-  //Operations like Add/Sum, Set/Max can also be implemented.
-  static inline T modify_op(const T & x, const T & y) {
-    return x + y;
+template<class T> class link_cut_forest {
+  static T join_values(const T &a, const T &b) {
+    return std::min(a, b);
   }
 
-  static inline T query_op(const T & x, const T & y) {
-    return std::max(x, y);
+  static T join_value_with_delta(const T &v, const T &d, int len) {
+    return d;
   }
 
-  static inline T delta_on_segment(const T & delta, int seglen) {
-    if (delta == null_delta()) return null_delta();
-    //Here you must write a fast equivalent of following slow code:
-    //  T result = delta;
-    //  for (int i = 1; i < seglen; i++) result = query_op(result, delta);
-    //  return result;
-    return delta;
-  }
-
-  static inline T null_delta() { return 0; }
-  static inline T null_value() { return INT_MIN; }
-
-  static inline T join_value_with_delta(const T & v, const T & delta) {
-    return delta == null_delta() ? v : modify_op(v, delta);
-  }
-
-  static T join_deltas(const T & delta1, const T & delta2) {
-    if (delta1 == null_delta()) return delta2;
-    if (delta2 == null_delta()) return delta1;
-    return modify_op(delta1, delta2);
+  static T join_deltas(const T &d1, const T &d2) {
+    return d2;  // For "set" updates, the more recent delta prevails.
   }
 
   struct node_t {
     T value, subtree_value, delta;
+    node_t *left, *right, *parent;
     int size;
-    bool rev;
-    node_t *L, *R, *parent;
+    bool rev, pending;
 
-    node_t(const T & v) {
-      value = subtree_value = v;
-      delta = null_delta();
-      size = 1;
-      rev = false;
-      L = R = parent = 0;
+    node_t(const T &v)
+        : value(v), subtree_value(v), left(NULL), right(NULL), parent(NULL),
+          size(1), rev(false), pending(false) {}
+
+    inline bool is_root() {
+      return parent == NULL || (parent->left != this && parent->right != this);
     }
 
-    bool is_root() { //is this the root of a splay tree?
-      return parent == 0 || (parent->L != this && parent->R != this);
+    inline T get_subtree_value() {
+      return pending ? join_value_with_delta(subtree_value, delta, size)
+                     : subtree_value;
     }
 
     void push() {
       if (rev) {
         rev = false;
-        std::swap(L, R);
-        if (L != 0) L->rev = !L->rev;
-        if (R != 0) R->rev = !R->rev;
+        std::swap(left, right);
+        if (left != NULL) {
+          left->rev = !left->rev;
+        }
+        if (right != NULL) {
+          right->rev = !right->rev;
+        }
       }
-      value = join_value_with_delta(value, delta);
-      subtree_value = join_value_with_delta(subtree_value,
-                        delta_on_segment(delta, size));
-      if (L != 0) L->delta = join_deltas(L->delta, delta);
-      if (R != 0) R->delta = join_deltas(R->delta, delta);
-      delta = null_delta();
+      if (pending) {
+        value = join_value_with_delta(value, delta, 1);
+        subtree_value = join_value_with_delta(subtree_value, delta, size);
+        if (left != NULL) {
+          left->delta = left->pending ? join_deltas(left->delta, delta) : delta;
+          left->pending = true;
+        }
+        if (right != NULL) {
+          right->delta = right->pending ? join_deltas(right->delta, delta)
+                                        : delta;
+          right->pending = true;
+        }
+        pending = false;
+      }
     }
 
     void update() {
-      subtree_value = query_op(query_op(get_subtree_value(L),
-                                        join_value_with_delta(value, delta)),
-                               get_subtree_value(R));
-      size = 1 + get_size(L) + get_size(R);
+      size = 1;
+      subtree_value = value;
+      if (left != NULL) {
+        subtree_value = join_values(subtree_value, left->get_subtree_value());
+        size += left->size;
+      }
+      if (right != NULL) {
+        subtree_value = join_values(subtree_value, right->get_subtree_value());
+        size += right->size;
+      }
     }
   };
 
-  static inline int get_size(node_t * n) {
-    return n == 0 ? 0 : n->size;
+  static void connect(node_t *child, node_t *parent, bool is_left) {
+    if (child != NULL) {
+      child->parent = parent;
+    }
+    if (is_left) {
+      parent->left = child;
+    } else {
+      parent->right = child;
+    }
   }
 
-  static inline int get_subtree_value(node_t * n) {
-    return n == 0 ? null_value() : join_value_with_delta(n->subtree_value,
-                                      delta_on_segment(n->delta, n->size));
+  static void rotate(node_t *n) {
+    node_t *parent = n->parent, *grandparent = parent->parent;
+    bool parent_is_root = parent->is_root(), is_left = (n == parent->left);
+    connect(is_left ? n->right : n->left, parent, is_left);
+    connect(parent, n, !is_left);
+    if (parent_is_root) {
+      if (n != NULL) {
+        n->parent = grandparent;
+      }
+    } else {
+      connect(n, grandparent, parent == grandparent->left);
+    }
+    parent->update();
   }
 
-  static void connect(node_t * ch, node_t * p, char is_left) {
-    if (ch != 0) ch->parent = p;
-    if (is_left < 0) return;
-    (is_left ? p->L : p->R) = ch;
-  }
-
-  /** rotates edge (n, n.parent)
-   *         g          g
-   *        /          /
-   *       p          n
-   *      / \  -->   / \
-   *     n  p.r    n.l  p
-   *    / \            / \
-   *  n.l n.r        n.r p.r
-   */
-  static void rotate(node_t * n) {
-    node_t *p = n->parent, *g = p->parent;
-    bool is_rootp = p->is_root(), is_left = (n == p->L);
-    connect(is_left ? n->R : n->L, p, is_left);
-    connect(p, n, !is_left);
-    connect(n, g, is_rootp ? -1 : (p == g->L));
-    p->update();
-  }
-
-  /** brings n to the root, balancing tree
-   *
-   *  zig-zig case:
-   *         g                                  n
-   *        / \               p                / \
-   *       p  g.r rot(p)    /   \     rot(n) n.l  p
-   *      / \      -->    n       g    -->       / \
-   *     n  p.r          / \     / \           n.r  g
-   *    / \            n.l n.r p.r g.r             / \
-   *  n.l n.r                                    p.r g.r
-   *
-   *  zig-zag case:
-   *       g               g
-   *      / \             / \               n
-   *     p  g.r rot(n)   n  g.r rot(n)    /   \
-   *    / \      -->    / \      -->    p       g
-   *  p.l  n           p  n.r          / \     / \
-   *      / \         / \            p.l n.l n.r g.r
-   *    n.l n.r     p.l n.l
-   */
-  static void splay(node_t * n) {
+  static void splay(node_t *n) {
     while (!n->is_root()) {
-      node_t *p = n->parent, *g = p->parent;
-      if (!p->is_root()) g->push();
-      p->push();
+      node_t *parent = n->parent, *grandparent = parent->parent;
+      if (!parent->is_root()) grandparent->push();
+      parent->push();
       n->push();
-      if (!p->is_root())
-        rotate((n == p->L) == (p == g->L) ? p/*zig-zig*/ : n/*zig-zag*/);
+      if (!parent->is_root()) {
+        if ((n == parent->left) == (parent == grandparent->left)) {
+          rotate(parent);  // zig-zig
+        } else {
+          rotate(n);  // zig-zag
+        }
+      }
       rotate(n);
     }
     n->push();
     n->update();
   }
 
-  //makes node n the root of the virtual tree,
-  //and also n becomes the leftmost node in its splay tree
-  static node_t * expose(node_t * n) {
-    node_t *prev = 0;
-    for (node_t *i = n; i != 0; i = i->parent) {
-      splay(i);
-      i->L = prev;
-      prev = i;
+  static node_t* expose(node_t *n) {
+    node_t *prev = NULL;
+    for (node_t *curr = n; curr != NULL; curr = curr->parent) {
+      splay(curr);
+      curr->left = prev;
+      prev = curr;
     }
     splay(n);
     return prev;
   }
 
-  std::map<int, node_t*> nodes; //use array if ID compression not required
-  node_t *u, *v; //temporary
+  std::map<int, node_t*> nodes;  // Use array if compression is not required.
+  node_t *u, *v;  // Helper variables.
+  int num_trees;
 
   void get_uv(int a, int b) {
-    static typename std::map<int, node_t*>::iterator it1, it2;
+    typename std::map<int, node_t*>::iterator it1, it2;
     it1 = nodes.find(a);
     it2 = nodes.find(b);
-    if (it1 == nodes.end() || it2 == nodes.end())
-      throw std::runtime_error("Error: a or b does not exist in forest.");
+    if (it1 == nodes.end() || it2 == nodes.end()) {
+      throw std::runtime_error("Queried node ID does not exist in forest.");
+    }
     u = it1->second;
     v = it2->second;
   }
 
  public:
-  ~linkcut_forest() {
-    static typename std::map<int, node_t*>::iterator it;
-    for (it = nodes.begin(); it != nodes.end(); ++it)
+  link_cut_forest() : num_trees(0) {}
+
+  ~link_cut_forest() {
+    typename std::map<int, node_t*>::iterator it;
+    for (it = nodes.begin(); it != nodes.end(); ++it) {
       delete it->second;
+    }
   }
 
-  void make_root(int id, const T & initv) {
-    if (nodes.find(id) != nodes.end())
-      throw std::runtime_error("Cannot make_root(): ID already exists.");
-    node_t * n = new node_t(initv);
+  int size() const {
+    return nodes.size();
+  }
+
+  int trees() const {
+    return num_trees;
+  }
+
+  void make_root(int i, const T &v = T()) {
+    if (nodes.find(i) != nodes.end()) {
+      throw std::runtime_error("Cannot make a root with an existing ID.");
+    }
+    node_t *n = new node_t(v);
     expose(n);
     n->rev = !n->rev;
-    nodes[id] = n;
+    nodes[i] = n;
+    num_trees++;
   }
 
-  bool connected(int a, int b) {
+  bool is_connected(int a, int b) {
     get_uv(a, b);
-    if (a == b) return true;
+    if (a == b) {
+      return true;
+    }
     expose(u);
     expose(v);
-    return u->parent != 0;
+    return u->parent != NULL;
   }
 
   void link(int a, int b) {
-    if (connected(a, b))
-      throw std::runtime_error("Error: a and b are already connected.");
+    if (is_connected(a, b)) {
+      throw std::runtime_error("Cannot link nodes that are already connected.");
+    }
     get_uv(a, b);
     expose(u);
     u->rev = !u->rev;
     u->parent = v;
+    num_trees--;
   }
 
   void cut(int a, int b) {
@@ -229,71 +257,85 @@ template<class T> class linkcut_forest {
     expose(u);
     u->rev = !u->rev;
     expose(v);
-    if (v->R != u || u->L != 0)
-      throw std::runtime_error("Error: edge (a, b) does not exist.");
-    v->R->parent = 0;
-    v->R = 0;
+    if (v->right != u || u->left != NULL) {
+      throw std::runtime_error("Cannot cut edge that does not exist.");
+    }
+    v->right->parent = NULL;
+    v->right = NULL;
+    num_trees++;
   }
 
   T query(int a, int b) {
-    if (!connected(a, b))
-      throw std::runtime_error("Error: a and b are not connected.");
+    if (!is_connected(a, b)) {
+      throw std::runtime_error("Cannot query nodes that are not connected.");
+    }
     get_uv(a, b);
     expose(u);
     u->rev = !u->rev;
     expose(v);
-    return get_subtree_value(v);
+    return v->get_subtree_value();
   }
 
-  void modify(int a, int b, const T & delta) {
-    if (!connected(a, b))
-      throw std::runtime_error("Error: a and b are not connected.");
+  void update(int a, int b, const T &d) {
+    if (!is_connected(a, b)) {
+      throw std::runtime_error("Cannot update nodes that are not connected.");
+    }
     get_uv(a, b);
     expose(u);
     u->rev = !u->rev;
     expose(v);
-    v->delta = join_deltas(v->delta, delta);
+    v->delta = v->pending ? join_deltas(v->delta, d) : d;
+    v->pending = true;
   }
 };
 
 /*** Example Usage ***/
 
-#include <iostream>
+#include <cassert>
 using namespace std;
 
 int main() {
-  linkcut_forest<int> F;
-/*
- v=10      v=40      v=20      v=10
-  0---------1---------2---------3
-                      \
-                       ---------4
-                               v=30
-*/
-  F.make_root(0, 10);
-  F.make_root(1, 40);
-  F.make_root(2, 20);
-  F.make_root(3, 10);
-  F.make_root(4, 30);
-  F.link(0, 1);
-  F.link(1, 2);
-  F.link(2, 3);
-  F.link(2, 4);
-  cout << F.query(1, 4) << "\n"; //40
-  F.modify(1, 1, -10);
-  F.modify(3, 4, -10);
-/*
- v=10      v=30      v=10      v=0
-  0---------1---------2---------3
-                      \
-                       ---------4
-                               v=20
-*/
-  cout << F.query(0, 4) << "\n"; //30
-  cout << F.query(3, 4) << "\n"; //20
-  F.cut(1, 2);
-  cout << F.connected(1, 2) << "\n"; //0
-  cout << F.connected(0, 4) << "\n"; //0
-  cout << F.connected(2, 3) << "\n"; //1
+  link_cut_forest<int> lcf;
+  lcf.make_root(0, 10);
+  lcf.make_root(1, 40);
+  lcf.make_root(2, 20);
+  lcf.make_root(3, 10);
+  lcf.make_root(4, 30);
+  assert(lcf.size() == 5);
+  assert(lcf.trees() == 5);
+  lcf.link(0, 1);
+  lcf.link(1, 2);
+  lcf.link(2, 3);
+  lcf.link(2, 4);
+  assert(lcf.trees() == 1);
+
+// v=10      v=40      v=20      v=10
+//  0---------1---------2---------3
+//                      |
+//                      ----------4
+//                               v=30
+  assert(lcf.query(1, 4) == 20);
+  lcf.update(1, 1, 100);
+  lcf.update(2, 4, 100);
+
+// v=10     v=100     v=100      v=10
+//  0---------1---------2---------3
+//                      |
+//                      ----------4
+//                              v=100
+  assert(lcf.query(4, 4) == 100);
+  assert(lcf.query(0, 4) == 10);
+  assert(lcf.query(3, 4) == 10);
+  lcf.cut(1, 2);
+
+// v=10     v=100     v=100      v=0
+//  0---------1         2---------3
+//                      |
+//                      ----------4
+//                              v=100
+  assert(lcf.trees() == 2);
+  assert(!lcf.is_connected(1, 2));
+  assert(!lcf.is_connected(0, 4));
+  assert(lcf.is_connected(2, 3));
   return 0;
 }
