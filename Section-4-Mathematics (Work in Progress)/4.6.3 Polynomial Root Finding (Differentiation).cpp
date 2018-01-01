44f2ca6d6883e0d2c04b1eb60ce04df92fd3a352
Alex Li <me@alexli.ca>
2017-12-31 22:18:34 -0500

    Updates

--- Section-4-Mathematics (Work in Progress)/4.6.3 Polynomial Root Finding (Differentiation).cpp
+++ Section-4-Mathematics (Work in Progress)/4.6.3 Polynomial Root Finding (Differentiation).cpp
@@ -1,97 +1,103 @@
 /*
 
-Real roots can be found via binary searching, a.k.a the bisection
-method. If two x-coordinates evaluate to y-coordinates that have
-opposite signs, a root must exist between them. For a polynomial
-function, at most 1 root lies between adjacent local extrema.
-Since local extrema exist where the derivative equals 0, we can
-break root-finding into the subproblem of finding the roots of
-the derivative. Recursively solve for local extrema until we get
-to a base case of degree 0. For each set of local extrema found,
-binary search between pairs of extrema for a root. This method is
-easy, robust, and allows us to find the root to an arbitrary level
-of accuracy. We're limited only by the precision of the arithmetic.
+Finds every root x for a polynomial p such that p(x) = 0 by differentiation.
+Each adjacent pair of local extrema is searched using the bisection method,
+where local extrema are recursively found by finding the root of the derivative.
 
-Complexity: For a degree N polynomial, repeatedly differentiating
-it will take N + (N-1) + ... + 1 = O(N^2) operations. At each step
-we binary search the number of times equal to the current degree.
-If we want to make roots precise to eps=10^-P, each binary search
-will take O(log P). Thus the overall complexity is O(N^2 log P).
+- horner_eval(p, x) evaluates the polynomial p of degree d (represented as a
+  vector of size d + 1 where p[i] stores the coefficient for the x^i term) at x,
+  using Horner's method.
+- find_one_root(p, a, b, EPS) returns a root in the interval [a, b] for a
+  polynomial p where sgn(f(a)) != sgn(f(b)), using the bisection method. If this
+  precondition is not satisfied, then NaN is returned. The root is found to a
+  tolerance of EPS in absolute or relative error (whichever is reached first).
+- find_all_roots(p, a, b, EPS) returns a vector of all roots in the interval
+  [a, b] for a polynomial p using the bisection method. The roots are found to a
+  tolerance of EPS in absolute or relative error (whichever is reached first).
+
+Time Complexity:
+- O(n) per call to horner_eval(), where n is the degree of the polynomial.
+- O(n log p) per call to find_one_root(), where n is the degree of the
+  polynomial and p = -log10(EPS) is the number of digits of absolute or relative
+  precision that is desired.
+- O(n^3 log p) per call to find_all_roots(), where n is the degree of the
+  polynomial and p = -log10(EPS) is the number of digits of absolute or relative
+  precision that is desired.
+
+Space Complexity:
+- O(1) auxiliary space for horner_eval() and find_one_root().
+- O(n^2) auxiliary heap and O(n) auxiliary stack space for find_all_roots(),
+  where n is the degree of the polynomial.
 
 */
 
-#include <cmath>   /* fabsl(), powl() */
-#include <limits>  /* std::numeric_limits<>::quiet_NaN() */
-#include <utility> /* std::pair<> */
+#include <cmath>
+#include <limits>
+#include <utility>
 #include <vector>
 
-typedef long double Double;
-typedef std::vector<std::pair<Double, int> > poly;
-
-const Double epsa = 1e-11; //required precision of roots in absolute error
-const Double epsr = 1e-15; //required precision of roots in relative error
-const Double eps0 = 1e-17; //x is considered a root if fabs(eval(x))<=eps0
-const Double inf = 1e20;   //[-inf, inf] is the range of roots to consider
-const Double NaN = std::numeric_limits<Double>::quiet_NaN();
-
-Double eval(const poly & p, Double x) {
-  Double res = 0;
-  for (int i = 0; i < (int)p.size(); i++)
-    res += p[i].first * powl(x, p[i].second);
+double horner_eval(const std::vector<double> &p, double x) {
+  double res = p.back();
+  for (int i = p.size() - 2; i >= 0; i--) {
+    res = res*x + p[i];
+  }
   return res;
 }
 
-Double find_root(const poly & p, Double x1, Double x2) {
-  Double y1 = eval(p, x1), y2 = eval(p, x2);
-  if (fabsl(y1) <= eps0) return x1;
-  bool neg1 = (y1 < 0), neg2 = (y2 < 0);
-  if (fabsl(y2) <= eps0 || neg1 == neg2) return NaN;
-  while (x2 - x1 > epsa && x1 * (1 + epsr) < x2 && x2 * (1 + epsr) > x1) {
-    Double x = (x1 + x2) / 2;
-    ((eval(p, x) < 0) == neg1 ? x1 : x2) = x;
+double find_one_root(const std::vector<double> &p, double a, double b,
+                     const double EPS = 1e-15) {
+  double pa = horner_eval(p, a), pb = horner_eval(p, b);
+  bool paneg = pa < 0, pbneg = pb < 0;
+  if (paneg == pbneg) {
+    return std::numeric_limits<double>::quiet_NaN();
   }
-  return x1;
+  while (b - a > EPS && a*(1 + EPS) < b && a < b*(1 + EPS)) {
+    double m = a + (b - a)/2;
+    if ((horner_eval(p, m) < 0) == paneg) {
+      a = m;
+    } else {
+      b = m;
+    }
+  }
+  return a;
 }
 
-std::vector<Double> find_all_roots(const poly & p) {
-  poly dif;
-  for (int i = 0; i < (int)p.size(); i++)
-    if (p[i].second > 0)
-      dif.push_back(std::make_pair(p[i].first * p[i].second, p[i].second - 1));
-  if (dif.empty()) return std::vector<Double>();
-  std::vector<Double> res, r = find_all_roots(dif);
-  r.insert(r.begin(), -inf);
-  r.push_back(inf);
-  for (int i = 0; i < (int)r.size() - 1; i++) {
-    Double root = find_root(p, r[i], r[i + 1]);
-    if (root != root) continue; //NaN, not found
-    if (res.empty() || root != res.back())
+std::vector<double> find_all_roots(const std::vector<double> &p,
+                                   double a = -1e20, double b = 1e20,
+                                   const double EPS = 1e-15) {
+  std::vector<double> pprime;
+  for (int i = 1; i < (int)p.size(); i++) {
+    pprime.push_back(p[i]*i);
+  }
+  if (pprime.empty()) {
+    return std::vector<double>();
+  }
+  std::vector<double> res, r = find_all_roots(pprime, a, b, EPS);
+  r.push_back(b);
+  for (int i = 0; i < (int)r.size(); i++) {
+    double root = find_one_root(p, i == 0 ? a : r[i - 1], r[i], EPS);
+    if (!std::isnan(root) && (res.empty() || root != res.back())) {
       res.push_back(root);
+    }
   }
   return res;
 }
 
-/*** Example Usage (http://wcipeg.com/problem/rootsolve) ***/
-
-#include <iostream>
+#include <cassert>
 using namespace std;
 
 int main() {
-  int n, d;
-  Double c;
-  poly p;
-  cin >> n;
-  for (int i = 0; i < n; i++) {
-    cin >> c >> d;
-    p.push_back(make_pair(c, d));
+  { // -1 + 2x - 6x^2 + 2x^3
+    int poly[] = {-1, 2, -6, 2};
+    vector<double> p(poly, poly + 4), roots = find_all_roots(p);
+    assert(roots.size() == 1 && fabs(horner_eval(p, roots[0])) < 1e-10);
   }
-  vector<Double> sol = find_all_roots(p);
-  if (sol.empty()) {
-    cout << "NO REAL ROOTS\n";
-  } else {
-    cout.precision(9);
-    for (int i = 0; i < (int)sol.size(); i++)
-      cout << fixed << sol[i] << "\n";
+  { // -20 + 4x + 3x^2
+    int poly[] = {-20, 4, 3};
+    vector<double> p(poly, poly + 3), roots = find_all_roots(p);
+    assert(roots.size() == 2);
+    assert(fabs(horner_eval(p, roots[0])) < 1e-10);
+    assert(fabs(horner_eval(p, roots[1])) < 1e-10);
   }
   return 0;
 }
