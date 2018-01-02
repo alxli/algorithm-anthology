de1569e35d4843d7b6bbdd7dea2103f0edbd804f
Alex Li <me@alexli.ca>
2018-01-02 03:00:21 -0500

    Updates

--- Section-4-Mathematics (Work in Progress)/4.7.1 Integration (Simpson's).cpp
+++ Section-4-Mathematics (Work in Progress)/4.7.1 Integration (Simpson's).cpp
@@ -1,40 +1,55 @@
 /*
 
-Simpson's rule is a method for numerical integration, the
-numerical approximation of definite integrals. The rule is:
+Computes the definite integral from a to b for a continuous function f using
+Simpson's approximation: integral ~ [f(a) + 4*f((a + b)/2) + f(b)]*(b - a)/6.
 
-Integral of f(x) dx from a to b ~=
-  [f(a) + 4*f((a + b)/2) + f(b)] * (b - a)/6
+- simpsons(f, a, b) returns the definite integral for a function f from a to b,
+  to a tolerance of EPS in absolute error.
+
+Time Complexity:
+- O(p) per call to integrate(), where p = -log10(EPS) is the number of digits
+  of absolute precision that is desired.
+
+Space Complexity:
+- O(p) auxiliary stack and O(1) auxiliary heap space, where p = -log10(EPS)
+  is the number of digits of absolute precision that is desired.
 
 */
 
-#include <cmath> /* fabs() */
+#include <cstdio>
+#include <cmath>
 
-template<class DoubleFunction>
-double simpsons(DoubleFunction f, double a, double b) {
-  return (f(a) + 4 * f((a + b)/2) + f(b)) * (b - a)/6;
+template<class ContinuousFunction>
+double simpsons(ContinuousFunction f, double a, double b) {
+  return (f(a) + 4*f((a + b)/2) + f(b))*(b - a)/6;
 }
 
-template<class DoubleFunction>
-double integrate(DoubleFunction f, double a, double b) {
-  static const double eps = 1e-10;
+template<class ContinuousFunction>
+double integrate(ContinuousFunction f, double a, double b,
+                 const double EPS = 1e-15) {
   double m = (a + b) / 2;
   double am = simpsons(f, a, m);
   double mb = simpsons(f, m, b);
   double ab = simpsons(f, a, b);
-  if (fabs(am + mb - ab) < eps) return ab;
+  printf("%.15lf\n", fabs(am + mb - ab));
+  if (fabs(am + mb - ab) < EPS) {
+    return ab;
+  }
   return integrate(f, a, m) + integrate(f, m, b);
 }
 
 /*** Example Usage ***/
 
-#include <iostream>
+#include <cstdio>
+#include <cassert>
 using namespace std;
 
-double f(double x) { return sin(x); }
+double f(double x) {
+  return sin(x);
+}
 
 int main () {
   double PI = acos(-1.0);
-  cout << integrate(f, 0.0, PI/2) << "\n"; //1
+  assert(fabs(integrate(f, 0.0, PI/2) - 1) < 1e-10);
   return 0;
 }
