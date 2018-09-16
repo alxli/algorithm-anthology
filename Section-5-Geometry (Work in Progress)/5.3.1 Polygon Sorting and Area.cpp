b3ff213a2745db61f8163d9d4187316694abdbe4
Alex Li <alextrovert@gmail.com>
2018-09-16 00:16:17 -0700

    Updates

--- Section-5-Geometry (Work in Progress)/5.3.1 Polygon Sorting and Area.cpp
+++ Section-5-Geometry (Work in Progress)/5.3.1 Polygon Sorting and Area.cpp
@@ -1,92 +1,115 @@
 /*
 
-centroid() - Simply returns the geometric average point of all the
-points given. This could be used to find the reference center point
-for the following function. An empty range will result in (0, 0).
-Complexity: O(n) on the number of points in the given range.
+Given a list of distinct two-dimensonal points, order them into a valid polygon
+and determine the area.
 
-cw_comp() - Given a set of points, these points could possibly form
-many different polygons. The following sorting comparators, when
-used in conjunction with std::sort, will produce one such ordering
-of points which is sorted in clockwise order relative to a custom-
-defined center point that must be set beforehand. This could very
-well be the result of mean_point(). ccw_comp() is the opposite
-function, which produces the points in counterclockwise order.
-Complexity: O(1) per call.
+- mean_center(lo, hi) returns the arithmetic mean of a range [lo, hi) of points,
+  where lo and hi must be RandomAccessIterators. This point is mathematically
+  guaranteed to lie with a non-self-intersecting closed polygon constructed by
+  sorting all other points clockwise about it. Note that this is different from
+  the geometric centroid (a.k.a. barycenter) of a polygon.
+- cw_comp(a, b, c) returns whether point a compares clockwise "before" point b
+  when using c as a central reference point.
+- cw_comp_class(c) constructs a wrapper class of cw_comp() that may be passed to
+  std::sort() a range of points clockwise to produce a valid polygon.
+- ccw_comp_class(c) constructs a wrapper class of cw_comp() that may be passed
+  to std::sort() a range of points counter-clockwise to produce a valid polygon.
+- polygon_area(lo, hi) returns the area of the polygon specified by the range
+  [lo, hi) of points, where lo and hi must be BidirectionalIterators. The points
+  are interpreted as a polygon based on the order given in the range. The input
+  polygon does not have to be sorted using the methods above, but must be given
+  in some ordering that yields a valid non-self-intersecting closed polygon.
+  Optionally, the last point may be equal to the first point in the input
+  without affecting the result. The area is computed using the shoelace formula.
 
-polygon_area() - A given range of points is interpreted as a polygon
-based on the ordering they're given in. The shoelace formula is used
-to determine its area. The polygon does not necessarily have to be
-sorted using one of the functions above, but may be any ordering that
-produces a valid polygon. You may optionally pass the last point in
-the range equal to the first point and still expect the correct result.
-Complexity: O(n) on the number of points in the range, assuming that
-the points are already sorted in the order that specifies the polygon.
+Time Complexity:
+- O(n) per call to mean_center(lo, hi) and polygon_area(lo, hi), where n is the
+  distance between lo and hi.
+- O(1) per call to cw_comp() and the related class comparators.
 
 */
 
-#include <algorithm> /* std::sort() */
-#include <cmath>     /* fabs() */
-#include <utility>   /* std::pair */
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <stdexcept>
+#include <utility>
+
+const double EPS = 1e-9;
+
+#define EQ(a, b) (fabs((a) - (b)) <= EPS)
+#define LT(a, b) ((a) < (b) - EPS)
+#define GE(a, b) ((a) >= (b) - EPS)
 
 typedef std::pair<double, double> point;
 #define x first
 #define y second
 
-const double eps = 1e-9;
+double sqnorm(const point &a) { return a.x*a.x + a.y*a.y; }
+double cross(const point &a, const point &b) { return a.x*b.y - a.y*b.x; }
 
-#define EQ(a, b) (fabs((a) - (b)) <= eps) /* equal to */
-#define LT(a, b) ((a) < (b) - eps)        /* less than */
-#define GE(a, b) ((a) >= (b) - eps)       /* greater than or equal to */
-
-//magnitude of the 3D cross product with Z component implicitly equal to 0
-//the answer assumes the origin (0, 0) is instead shifted to point o.
-//this is equal to 2x the signed area of the triangle from these 3 points.
-double cross(const point & o, const point & a, const point & b) {
-  return (a.x - o.x) * (b.y - o.y) - (a.y - o.y) * (b.x - o.x);
-}
-
-point ctr;
-
-template<class It> point centroid(It lo, It hi) {
-  if (lo == hi) return point(0, 0);
-  double xtot = 0, ytot = 0, points = hi - lo;
-  for (; lo != hi; ++lo) {
-    xtot += lo->x;
-    ytot += lo->y;
+template<class It>
+point mean_center(It lo, It hi) {
+  if (lo == hi) {
+    throw std::runtime_error("Cannot get center of an empty range.");
   }
-  return point(xtot / points, ytot / points);
+  double x_sum = 0, y_sum = 0, num_points = hi - lo;
+  for (; lo != hi; ++lo) {
+    x_sum += lo->x;
+    y_sum += lo->y;
+  }
+  return point(x_sum / num_points, y_sum / num_points);
 }
 
-//ctr must be defined beforehand
-bool cw_comp(const point & a, const point & b) {
-  if (GE(a.x - ctr.x, 0) && LT(b.x - ctr.x, 0)) return true;
-  if (LT(a.x - ctr.x, 0) && GE(b.x - ctr.x, 0)) return false;
-  if (EQ(a.x - ctr.x, 0) && EQ(b.x - ctr.x, 0)) {
-    if (GE(a.y - ctr.y, 0) || GE(b.y - ctr.y, 0))
+bool cw_comp(const point &a, const point &b, const point &c) {
+  if (GE(a.x - c.x, 0) && LT(b.x - c.x, 0)) {
+    return true;
+  }
+  if (LT(a.x - c.x, 0) && GE(b.x - c.x, 0)) {
+    return false;
+  }
+  if (EQ(a.x - c.x, 0) && EQ(b.x - c.x, 0)) {
+    if (GE(a.y - c.y, 0) || GE(b.y - c.y, 0)) {
       return a.y > b.y;
+    }
     return b.y > a.y;
   }
-  double det = cross(ctr, a, b);
-  if (EQ(det, 0))
-    return (a.x - ctr.x) * (a.x - ctr.x) + (a.y - ctr.y) * (a.y - ctr.y) >
-           (b.x - ctr.x) * (b.x - ctr.x) + (b.y - ctr.y) * (b.y - ctr.y);
+  point ac(a.x - c.x, a.y - c.y), bc(b.x - c.x, b.y - c.y);
+  double det = cross(ac, bc);
+  if (EQ(det, 0)) {
+    return sqnorm(ac) > sqnorm(bc);
+  }
   return det < 0;
 }
 
-bool ccw_comp(const point & a, const point & b) {
-  return cw_comp(b, a);
-}
+struct cw_comp_class {
+  point c;
+  cw_comp_class(const point &c) : c(c) {}
+  bool operator()(const point &a, const point &b) const {
+    return cw_comp(a, b, c);
+  }
+};
 
-//area of a polygon specified by range [lo, hi) - shoelace formula in O(n)
-//[lo, hi) must point to the polygon vertices, sorted in CW or CCW order
-template<class It> double polygon_area(It lo, It hi) {
-  if (lo == hi) return 0;
+struct ccw_comp_class {
+  point c;
+  ccw_comp_class(const point &c) : c(c) {}
+  bool operator()(const point &a, const point &b) const {
+    return cw_comp(b, a, c);
+  }
+};
+
+template<class It>
+double polygon_area(It lo, It hi) {
+  if (lo == hi) {
+    return 0;
+  }
   double area = 0;
-  if (*lo != *--hi)
-    area += (lo->x - hi->x) * (lo->y + hi->y);
-  for (It i = hi, j = hi - 1; i != lo; --i, --j)
-    area += (i->x - j->x) * (i->y + j->y);
+  if (*lo != *--hi) {
+    area += (lo->x - hi->x)*(lo->y + hi->y);
+  }
+  for (It i = hi, j = --hi; i != lo; --i, --j) {
+    area += (i->x - j->x)*(i->y + j->y);
+  }
   return fabs(area / 2.0);
 }
 
@@ -95,19 +118,21 @@ template<class It> double polygon_area(It lo, It hi) {
 #include <cassert>
 #include <vector>
 using namespace std;
-#define pt point
 
 int main() {
-  //irregular pentagon with only (1, 2) not on the convex hull
-  //the ordering here is already sorted in ccw order around their centroid
-  //we will scramble them and see if our comparator works
-  pt pts[] = {pt(1, 3), pt(1, 2), pt(2, 1), pt(0, 0), pt(-1, 3)};
-  vector<pt> v(pts, pts + 5);
+  // Irregular pentagon with only the vertex (1, 2) not on its convex hull.
+  // The ordering here is already sorted in ccw order around their mean center,
+  // though we will shuffle them to verify our sorting comparator.
+  point points[] = {point(1, 3), point(1, 2), point(2, 1), point(0, 0),
+                 point(-1, 3)};
+  vector<point> v(points, points + 5);
   std::random_shuffle(v.begin(), v.end());
-  ctr = centroid(v.begin(), v.end()); //note: ctr is a global variable
-  assert(EQ(ctr.x, 0.6) && EQ(ctr.y, 1.8));
-  sort(v.begin(), v.end(), cw_comp);
-  for (int i = 0; i < (int)v.size(); i++) assert(v[i] == pts[i]);
+  point c = mean_center(v.begin(), v.end());
+  assert(EQ(c.x, 0.6) && EQ(c.y, 1.8));
+  sort(v.begin(), v.end(), cw_comp_class(c));
+  for (int i = 0; i < (int)v.size(); i++) {
+    assert(v[i] == points[i]);
+  }
   assert(EQ(polygon_area(v.begin(), v.end()), 5));
   return 0;
 }
