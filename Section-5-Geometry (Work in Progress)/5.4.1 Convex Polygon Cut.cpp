7d5f2f2a120d16d145d1140a123ec5bf8f5cadb3
Alex Li <alextrovert@gmail.com>
2018-12-15 05:48:35 -0500

    Update 5.4.1 Convex Polygon Cut.cpp

--- Section-5-Geometry (Work in Progress)/5.4.1 Convex Polygon Cut.cpp
+++ Section-5-Geometry (Work in Progress)/5.4.1 Convex Polygon Cut.cpp
@@ -1,58 +1,76 @@
 /*
 
-Given a range of points specifying a polygon on the Cartesian
-plane, as well as two points specifying an infinite line, "cut"
-off the right part of the polygon with the line and return the
-resulting polygon that is the left part.
+Given a convex polygon (a polygon such that every line crossing through it will
+only do so once) in two dimensions, and two points specifying an infinite line,
+cut off the right part of the polygon, and return the resulting left part.
 
-Time Complexity: O(n) on the number of points in the poylgon.
+- convex_cut(lo, hi, p, q) returns the points of the left side of a polygon, in
+  clockwise order, after it has been cut by the line containing points p and q.
+  The original convex polygon is given by the range [lo, hi) of points in
+  clockwise order, where lo and hi must be RandomAccessIterators.
+
+Time Complexity:
+- O(n) per call to convex_cut(lo, hi, p, q), where n is the distance between lo
+  and hi.
+
+Space Complexity:
+- O(n) auxiliary for storage of the resulting convex cut.
 
 */
 
-#include <cmath>   /* fabs() */
-#include <utility> /* std::pair */
+#include <cmath>
+#include <cstdlib>
+#include <utility>
 #include <vector>
+
+const double EPS = 1e-9;
+
+#define EQ(a, b) (fabs((a) - (b)) <= EPS)
+#define LT(a, b) ((a) < (b) - EPS)
+#define GT(a, b) ((a) > (b) + EPS)
 
 typedef std::pair<double, double> point;
 #define x first
 #define y second
 
-const double eps = 1e-9;
-
-#define EQ(a, b) (fabs((a) - (b)) <= eps) /* equal to */
-#define LT(a, b) ((a) < (b) - eps)        /* less than */
-#define GT(a, b) ((a) > (b) + eps)        /* greater than */
-
-double cross(const point & o, const point & a, const point & b) {
-  return (a.x - o.x) * (b.y - o.y) - (a.y - o.y) * (b.x - o.x);
+double cross(const point &a, const point &b, const point &o = point(0, 0)) {
+  return (a.x - o.x)*(b.y - o.y) - (a.y - o.y)*(b.x - o.x);
 }
 
-int orientation(const point & o, const point & a, const point & b) {
-  double c = cross(o, a, b);
+int turn(const point &a, const point &o, const point &b) {
+  double c = cross(a, b, o);
   return LT(c, 0) ? -1 : (GT(c, 0) ? 1 : 0);
 }
 
-int line_intersection(const point & p1, const point & p2,
-                      const point & p3, const point & p4, point * p = 0) {
+int line_intersection(const point &p1, const point &p2,
+                      const point &p3, const point &p4, point *p = NULL) {
   double a1 = p2.y - p1.y, b1 = p1.x - p2.x;
-  double c1 = -(p1.x * p2.y - p2.x * p1.y);
+  double c1 = -(p1.x*p2.y - p2.x*p1.y);
   double a2 = p4.y - p3.y, b2 = p3.x - p4.x;
-  double c2 = -(p3.x * p4.y - p4.x * p3.y);
-  double x = -(c1 * b2 - c2 * b1), y = -(a1 * c2 - a2 * c1);
-  double det = a1 * b2 - a2 * b1;
-  if (EQ(det, 0))
+  double c2 = -(p3.x*p4.y - p4.x*p3.y);
+  double x = -(c1*b2 - c2*b1), y = -(a1*c2 - a2*c1);
+  double det = a1*b2 - a2*b1;
+  if (EQ(det, 0)) {
     return (EQ(x, 0) && EQ(y, 0)) ? 1 : -1;
-  if (p != 0) *p = point(x / det, y / det);
+  }
+  if (p != NULL) {
+    *p = point(x / det, y / det);
+  }
   return 0;
 }
 
 template<class It>
-std::vector<point> convex_cut(It lo, It hi, const point & p, const point & q) {
+std::vector<point> convex_cut(It lo, It hi, const point &p, const point &q) {
+  if (EQ(p.x, q.x) && EQ(p.y, q.y)) {
+    throw std::runtime_error("Cannot cut using line from identical points.");
+  }
   std::vector<point> res;
   for (It i = lo, j = hi - 1; i != hi; j = i++) {
-    int d1 = orientation(p, q, *j), d2 = orientation(p, q, *i);
-    if (d1 >= 0) res.push_back(*j);
-    if (d1 * d2 < 0) {
+    int d1 = turn(q, p, *j), d2 = turn(q, p, *i);
+    if (d1 >= 0) {
+      res.push_back(*j);
+    }
+    if (d1*d2 < 0) {
       point r;
       line_intersection(p, q, *j, *i, &r);
       res.push_back(r);
@@ -63,21 +81,39 @@ std::vector<point> convex_cut(It lo, It hi, const point & p, const point & q) {
 
 /*** Example Usage ***/
 
-#include <iostream>
+#include <cassert>
 using namespace std;
 
 int main() {
-  //irregular pentagon with only (1, 2) not on the convex hull
-  vector<point> v;
-  v.push_back(point(1, 3));
-  v.push_back(point(1, 2));
-  v.push_back(point(2, 1));
-  v.push_back(point(0, 0));
-  v.push_back(point(-1, 3));
-  //cut using the vertical line through (0, 0)
-  vector<point> res = convex_cut(v.begin(), v.end(), point(0, 0), point(0, 1));
-  cout << "left cut:\n";
-  for (int i = 0; i < (int)res.size(); i++)
-    cout << "(" << res[i].x << "," << res[i].y << ")\n";
+  {
+    vector<point> v;
+    v.push_back(point(1, 3));
+    v.push_back(point(2, 2));
+    v.push_back(point(2, 1));
+    v.push_back(point(0, 0));
+    v.push_back(point(-1, 3));
+    // Cut using the vertical line through (0, 0).
+    vector<point> c;
+    c.push_back(point(-1, 3));
+    c.push_back(point(0, 3));
+    c.push_back(point(0, 0));
+    assert(convex_cut(v.begin(), v.end(), point(0, 0), point(0, 1)) == c);
+  }
+  { // On a non-convex input, the result may be multiple disjoint polygons!
+    vector<point> v;
+    v.push_back(point(0, 0));
+    v.push_back(point(2, 2));
+    v.push_back(point(0, 4));
+    v.push_back(point(3, 4));
+    v.push_back(point(3, 0));
+    vector<point> c;
+    c.push_back(point(1, 0));
+    c.push_back(point(0, 0));
+    c.push_back(point(1, 1));
+    c.push_back(point(1, 3));
+    c.push_back(point(0, 4));
+    c.push_back(point(1, 4));
+    assert(convex_cut(v.begin(), v.end(), point(1, 0), point(1, 4)) == c);
+  }
   return 0;
 }
