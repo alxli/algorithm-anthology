97e1617fbe17ed4553cd76e6bca6a92ae3f2e470
Alex Li <me@alexli.ca>
2023-09-25 02:44:05 -0700

    Update 4.3.3_Bridges,_Cut-points,_and_Biconnectivity.cpp

--- 4-Graphs/4.3.3_Bridges,_Cut-points,_and_Biconnectivity.cpp
+++ 4-Graphs/4.3.3_Bridges,_Cut-points,_and_Biconnectivity.cpp
@@ -5,24 +5,20 @@ Tarjan's algorithm. tarjan() applies to a global, pre-populated adjacency list
 adj[] which satisfies the precondition that for every node v in adj[u], node u
 also exists in adj[v]. Nodes in adj[] must be numbered with integers between 0
 (inclusive) and the total number of nodes (exclusive), as passed in the function
-arguments. get_block_forest() applies to the global vector of biconnected
-components bcc[] which must have already been precomputed by a call to tarjan().
+arguments. get_block_forest() applies to a global vector blocks[] which must be
+already precomputed by a call to tarjan().
+
+A cut-point (i.e. cut-node, or articulation point) is any node whose removal
+increases the number of connected components in the graph.
 
 A bridge is an edge such that when deleted, the number of connected components
 in the graph is increased. An edge is a bridge if and only if it is not part of
 any cycle.
 
-A cut-point (i.e. cut-node, or articulation point) is any node whose removal
-increases the number of connected components in the graph.
-
-A biconnected component of a graph is a maximally biconnected subgraph. A
-biconnected graph is a connected and "non-separable" graph, meaning that if any
-node were to be removed, the graph will remain connected. Thus, a biconnected
-graph has no articulation points.
-
-Any connected graph decomposes into a tree of biconnected components called the
-"block tree" of the graph. An unconnected graph will thus decompose into a
-"block forest."
+Condensing each maximal component without a bridge into a single node, we can
+decompose any connected graph into a bridge-block tree (a.k.a. bridge-tree or
+block-tree), with bridges connecting each block. An unconnected graph will thus
+decompose into a "bridge-block forest."
 
 Time Complexity:
 - O(max(n, m)) per call to tarjan() and get_block_forest(), where n is the
@@ -42,9 +38,9 @@ Space Complexity:
 const int MAXN = 100;
 int timer, lowlink[MAXN], tin[MAXN], comp[MAXN];
 std::vector<bool> visit(MAXN);
-std::vector<int> adj[MAXN], bcc_forest[MAXN];
+std::vector<int> adj[MAXN], block_forest[MAXN];
 std::vector<int> stack, cutpoints;
-std::vector<std::vector<int> > bcc;
+std::vector<std::vector<int> > block;
 std::vector<std::pair<int, int> > bridges;
 
 void dfs(int u, int p) {
@@ -83,12 +79,12 @@ void dfs(int u, int p) {
       stack.pop_back();
       component.push_back(v);
     } while (u != v);
-    bcc.push_back(component);
+    block.push_back(component);
   }
 }
 
 void tarjan(int nodes) {
-  bcc.clear();
+  block.clear();
   bridges.clear();
   cutpoints.clear();
   stack.clear();
@@ -106,17 +102,17 @@ void tarjan(int nodes) {
 void get_block_forest(int nodes) {
   std::fill(comp, comp + nodes, 0);
   for (int i = 0; i < nodes; i++) {
-    bcc_forest[i].clear();
+    block_forest[i].clear();
   }
-  for (int i = 0; i < (int)bcc.size(); i++) {
-    for (int j = 0; j < (int)bcc[i].size(); j++) {
-      comp[bcc[i][j]] = i;
+  for (int i = 0; i < (int)block.size(); i++) {
+    for (int j = 0; j < (int)block[i].size(); j++) {
+      comp[block[i][j]] = i;
     }
   }
   for (int i = 0; i < nodes; i++) {
     for (int j = 0; j < (int)adj[i].size(); j++) {
       if (comp[i] != comp[adj[i][j]]) {
-        bcc_forest[comp[i]].push_back(comp[adj[i][j]]);
+        block_forest[comp[i]].push_back(comp[adj[i][j]]);
       }
     }
   }
@@ -129,14 +125,14 @@ Bridges:
 1 2
 5 4
 3 7
-Edge-Biconnected Components:
-2
-4
-5 1 0
-7
-3
-6
-Adjacency List for Block Forest:
+Blocks, a.k.a. Edge-Biconnected Components:
+2 
+4 
+5 1 0 
+7 
+3 
+6 
+Adjacency List for Bridge-Block Forest:
 0 => 2
 1 => 2
 2 => 0 1
@@ -171,18 +167,18 @@ int main() {
   for (int i = 0; i < (int)bridges.size(); i++) {
     cout << bridges[i].first << " " << bridges[i].second << endl;
   }
-  cout << "Edge-Biconnected Components:" << endl;
-  for (int i = 0; i < (int)bcc.size(); i++) {
-    for (int j = 0; j < (int)bcc[i].size(); j++) {
-      cout << bcc[i][j] << " ";
+  cout << "Blocks, or Edge-Biconnected Components:" << endl;
+  for (int i = 0; i < (int)block.size(); i++) {
+    for (int j = 0; j < (int)block[i].size(); j++) {
+      cout << block[i][j] << " ";
     }
     cout << endl;
   }
-  cout << "Adjacency List for Block Forest:" << endl;
-  for (int i = 0; i < (int)bcc.size(); i++) {
+  cout << "Adjacency List for Bridge-Block Forest:" << endl;
+  for (int i = 0; i < (int)block.size(); i++) {
     cout << i << " =>";
-    for (int j = 0; j < (int)bcc_forest[i].size(); j++) {
-      cout << " " << bcc_forest[i][j];
+    for (int j = 0; j < (int)block_forest[i].size(); j++) {
+      cout << " " << block_forest[i][j];
     }
     cout << endl;
   }
